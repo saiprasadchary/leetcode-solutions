92c14022fc662548b9e99e432967a4870f240451
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-13 04:18:40 -0400

    Time: 314 ms (14.74%), Space: 48.6 MB (5.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,18 +7,40 @@ class Solution(object):
         str2="";
         for i in s:
             if i.isalnum():
-                if(i.isalpha() and i.isupper()):
-                    str2+=i.lower();
-                else:
-                    str2+=i
+                str2+=i.lower();
+            
+        print(str2)
+        def PalindromeCheck(n,str2):
+            if n>=len(str2)//2:
+                return True;
+            if str2[n]!=str2[len(str2)-n-1]:
+                return False;
+     
+            return PalindromeCheck(n+1,str2)
+        return PalindromeCheck(0,str2)  
+        
+        
 
-        c=0;
-        for i in range(len(str2)//2):
-            if(str2[i]==str2[len(str2)-i-1]):
-                c+=1
-        print(c)
-        if c==len(str2)//2:
-            return True;
-        else:
-            return False
+
+
+
+
+
+        # str2="";
+        # for i in s:
+        #     if i.isalnum():
+        #         if(i.isalpha() and i.isupper()):
+        #             str2+=i.lower();
+        #         else:
+        #             str2+=i
+
+        # c=0;
+        # for i in range(len(str2)//2):
+        #     if(str2[i]==str2[len(str2)-i-1]):
+        #         c+=1
+        # print(c)
+        # if c==len(str2)//2:
+        #     return True;
+        # else:
+        #     return False
 
