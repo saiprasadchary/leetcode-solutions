ea83efe9d9af2955e3c67ae893de207f9e3abb40
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-13 20:14:05 -0400

    Time: 16 ms (59.52%), Space: 11.6 MB (29.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    def __init__(self):
-        self.mem={0:0,1:1}
+    # def __init__(self):
+    #     self.mem={0:0,1:1}
     def fib(self, n):
         # if(n==1):
         #     return 1
@@ -8,6 +8,20 @@ class Solution(object):
         #     return 0
         # return self.fib(n-1)+self.fib(n-2)
         #Next solve it using memoization
-        if n not in self.mem:
-            self.mem[n]=self.fib(n-1)+self.fib(n-2);
-        return self.mem[n]
+        # if n not in self.mem:
+        #     self.mem[n]=self.fib(n-1)+self.fib(n-2);
+        # return self.mem[n]
+        arr=[]
+        if n==0:
+            return 0;
+        if n==1:
+            return 1
+                  
+        secLast=0;
+        last=1;
+        for i in range(2,n+1):
+            curr=last+secLast;
+            secLast=last;
+            last=curr
+        return last
+
