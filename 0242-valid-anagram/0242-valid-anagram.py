3d3603c709caf2dcdefda255f6208126c10e252c
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-16 03:25:35 -0400

    Time: 32 ms (64.47%), Space: 12 MB (82.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,9 +7,20 @@ class Solution(object):
         """
         if len(s)!=len(t):
             return False
-        s1=sorted(s)
-        t1=sorted(t);
-        if s1==t1:
-            return True;
-        else:
-            return False;
+        dict1={};
+        dict2={};
+        
+        for i in s:
+            if i not in dict1:
+                dict1[i]=1;
+            else:
+                dict1[i]+=1;
+
+        for j in t:
+            if j not in dict2:
+                dict2[j]=1;
+            else:
+                dict2[j]+=1;
+        
+        
+        return dict1==dict2        
