6638d61d3688e44c9dc9c129ccf5a72a66d735f6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-18 18:03:50 -0400

    Time: 0 ms (100%), Space: 11.5 MB (86.61%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -5,22 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        dict_s = {}
+        # dict_s = {}
 
-        # couting the chars in the base dictionary i.e dict_s:
-        for i in s:
-            if i not in dict_s:
-                dict_s[i]=1;
-            else:
-                dict_s[i]+=1;
+        # # couting the chars in the base dictionary i.e dict_s:
+        # for i in s:
+        #     if i not in dict_s:
+        #         dict_s[i]=1;
+        #     else:
+        #         dict_s[i]+=1;
         
-        # check if the count of the ele in the t is present or not;
+        # # check if the count of the ele in the t is present or not;
 
-        for j in t:
-            if j not in dict_s or dict_s[j]==0:
-                return j;
-            else:
-                # negate the cout in the existing dictionary
-                dict_s[j]-=1;
+        # for j in t:
+        #     if j not in dict_s or dict_s[j]==0:
+        #         return j;
+        #     else:
+        #         # negate the cout in the existing dictionary
+        #         dict_s[j]-=1;
+        x=0;
+        for i in s+t:
+            x=x^ord(i)
+        return chr(x)
             
 
