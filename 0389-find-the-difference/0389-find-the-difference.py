07d4280561885cd29c33f3e3a61deeec6a9fcec1
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-18 10:02:57 -0400

    Time: 4 ms (99.77%), Space: 11.6 MB (86.42%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -7,16 +7,20 @@ class Solution(object):
         """
         dict_s = {}
 
-        # Count frequencies in s
+        # couting the chars in the base dictionary i.e dict_s:
         for i in s:
             if i not in dict_s:
-                dict_s[i] = 1
+                dict_s[i]=1;
             else:
-                dict_s[i] += 1
+                dict_s[i]+=1;
+        
+        # check if the count of the ele in the t is present or not;
 
-        # Check against frequencies in t
-        for i in t:
-            if i not in dict_s or dict_s[i] == 0:
-                return i
+        for j in t:
+            if j not in dict_s or dict_s[j]==0:
+                return j;
             else:
-                dict_s[i] -= 1
+                # negate the cout in the existing dictionary
+                dict_s[j]-=1;
+            
+
