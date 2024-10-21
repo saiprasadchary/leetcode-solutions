3a6afec444fa35c9356a5cc696d58fc5aa1356bd
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-21 18:41:37 -0400

    Time: 11 ms (97.38%), Space: 11.7 MB (80.19%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -7,9 +7,21 @@ class Solution(object):
         """
         l1=[];
         for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]==nums2[j]:
-                    l1.append(nums1[i]);
-                    nums2[j]=None;
-                    break
-        return l1
+            if nums1[i] in nums2:
+                l1.append(nums1[i]);
+                nums2.remove(nums1[i])
+                
+        return (l1)
+
+
+
+
+
+        # l1=[];
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i]==nums2[j]:
+        #             l1.append(nums1[i]);
+        #             nums2[j]=None;
+        #             break
+        # return l1
