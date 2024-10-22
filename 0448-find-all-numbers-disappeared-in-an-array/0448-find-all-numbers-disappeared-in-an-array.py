336caff278785beae109c9180de9a258925aec27
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-22 19:41:57 -0400

    Time: 28 ms (96.85%), Space: 19.8 MB (66.36%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -5,13 +5,20 @@ class Solution(object):
         :rtype: List[int]
         """
         # Create a set of all numbers that should appear in the array
-        num_set=set();
-        for x in range(1, len(nums)+1):
-            num_set.add(x)  
-        # Remove each number that appears in nums
-        for num in (nums):
-            if num in num_set:
-                num_set.remove(num)
+        # num_set=set();
+        # for x in range(1, len(nums)+1):
+        #     num_set.add(x)  
+        # # Remove each number that appears in nums
+        # for num in (nums):
+        #     if num in num_set:
+        #         num_set.remove(num)
         
-        # The remaining numbers in num_set are the missing numbers
-        return list(num_set)
+        # # The remaining numbers in num_set are the missing numbers
+        # return list(num_set)
+        set_nums=set(nums)
+
+        l1=[];
+        for i in range(1,len(nums)+1):
+            if i not in set_nums:
+                l1.append(i)
+        return l1
