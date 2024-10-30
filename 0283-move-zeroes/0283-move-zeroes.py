24820dae95e08e1164c54d3e04c21625cc301e94
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-30 14:45:47 -0400

    Time: 10 ms (43.39%), Space: 12.8 MB (50.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,18 +1,17 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        i = 0  # Pointer for placing non-zero elements
-        j = 1  # Pointer for scanning the array
+        j=0;
+        for i in range(1, len(nums)):
+            if nums[i]!=0 and nums[j]==0:
+                nums[i], nums[j]=nums[j],nums[i];
+                j+=1;
+            elif nums[i]==0 and nums[j]==0:
+                continue
+            else:
+                j+=1;
+        
 
-        # Special handling for length 2 is removed for generality and simplicity
-        while j < len(nums):
-            if nums[i] == 0 and nums[j] != 0:
-                # Perform swap if current is zero and jth is non-zero
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1  # Move i to the next index for the next potential non-zero placement
-            elif nums[i] != 0:
-                i += 1  # If nums[i] is non-zero, just move i to catch up with j if needed
-            j += 1 
+
+
+
+            
