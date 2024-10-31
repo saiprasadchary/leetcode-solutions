3dbadd1efdea02f3158e1fc6f8d24ce6f4ff01e3
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-31 14:48:27 -0400

    Time: 11 ms (43.75%), Space: 13.5 MB (7.4%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,10 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxi=max(nums);
-        for i in range(maxi):
-            print(i)
-            if i not in nums:
-                return i;
-        return maxi+1;
+        # maxi=max(nums);
+        # for i in range(maxi):
+        #     print(i)
+        #     if i not in nums:
+        #         return i;
+        # return maxi+1;
+        seen={}
+        for i in range(len(nums)):
+            seen[nums[i]]=1;
         
+        for i in range(len(nums)):
+            if i not in seen:
+                return i
+        return i+1
