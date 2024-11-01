d4f316b5da4f1514cdc1591a9935d5e4c17ef595
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-10-31 21:09:12 -0400

    Time: 782 ms (6.09%), Space: 15.4 MB (5.76%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -9,11 +9,16 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i]!=1:
                 c=0;
-                maxi=max(maxi,c)
+                if maxi<c:
+                    maxi=c;
                 
             else:
                 c+=1
                 maxi=max(maxi,c)
+                if maxi<c:
+                    maxi=c;
+
+
                 print(maxi)
         return maxi
         
