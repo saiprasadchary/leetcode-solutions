32f4aa107c768e6f057f5a6dd32abd1fde57c2e5
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-02 17:09:57 -0400

    Time: 0 ms (100%), Space: 11.8 MB (7.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,29 +4,54 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        count_0=0;
-        count_1=0;
-        count_2=0;
 
-        for i in range(len(nums)):
-            if nums[i]==0:
-                count_0+=1;
-            elif nums[i]==1:
-                count_1+=1;
+        #optimal:  Dutch National Flag Algo;;
+
+        low, mid=0,0;
+        n=len(nums);
+        high=n-1;
+
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low], nums[mid]= nums[mid], nums[low];
+                low+=1;
+                mid+=1;
+            elif nums[mid]==1:
+                mid+=1;
             else:
-                count_2+=1;
+                nums[mid], nums[high]= nums[high], nums[mid];
+                high-=1;
+
+        return nums
+
+
+
+        # Three 0,1,2 approach and replacing (inplace)
+        # count_0=0;
+        # count_1=0;
+        # count_2=0;
+
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         count_0+=1;
+        #     elif nums[i]==1:
+        #         count_1+=1;
+        #     else:
+        #         count_2+=1;
         
-        # inplacing 0's;
-        for i in range(count_0):
-            nums[i]=0;
+        # # inplacing 0's;
+        # for i in range(count_0):
+        #     nums[i]=0;
+            
+        # # inplacing 1's;
+        # for j in range(count_0, count_0 + count_1):
+        #     nums[j]=1;
+
+        # # inplacing 2's;
+        # for k in range(count_0+count_1 , count_0+count_1+count_2):
+        #     nums[k]=2;
         
-        for j in range(count_0, count_0 + count_1):
-            nums[j]=1;
-        
-        for k in range(count_0+count_1 , count_0+count_1+count_2):
-            nums[k]=2;
-        
-        return nums;
+        # return nums;
 
 
 
