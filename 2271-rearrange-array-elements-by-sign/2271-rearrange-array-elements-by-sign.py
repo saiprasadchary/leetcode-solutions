7278d4d7b85cb81ca1acb91a7851118347affca0
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-06 13:44:56 -0500

    Time: 106 ms (43.93%), Space: 43.3 MB (80.6%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -17,22 +17,38 @@ class Solution(object):
 
         # Interleave positive and negative numbers
         i, j, k = 0, 0, 0  # 'i' for pos_arr, 'j' for neg_arr, 'k' for ans
-        while i < len(pos_arr) and j < len(neg_arr):
-            ans[k] = pos_arr[i]
-            ans[k+1] = neg_arr[j]
-            i += 1
-            j += 1
-            k += 2
-
-        # If there are leftover positives or negatives due to unequal lengths
-        while i < len(pos_arr):
-            ans[k] = pos_arr[i]
-            i += 1
-            k += 1
-        
-        while j < len(neg_arr):
-            ans[k] = neg_arr[j]
-            j += 1
-            k += 1
-
+        while k < len(pos_arr):
+            ans[2*k]= pos_arr[k];
+            ans[2*k +1] = neg_arr[k];
+            k+=1
         return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
