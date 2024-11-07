a9bb2d1b3cc3e9129ca0b7ce13246a61374e8810
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-07 17:04:10 -0500

    Time: 1408 ms (37.47%), Space: 21.4 MB (96.26%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,17 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        s=set(nums);
-        longest=0;
-        for i in range(len(nums)):
-            if nums[i]-1 not in s:
-                c=1;
-                k=nums[i]+1
-
-                while k in s:
-                    c+=1;
-                    k+=1
+        if len(nums)==0:
+            return 0
+        longest=1;
+        c=1
+        s_nums=sorted(nums);
+        print(s_nums)
+        for i in range(1, len(nums)):
+            start=s_nums[i]
+            if s_nums[i]==s_nums[i-1]:
+                continue;
+            elif s_nums[i-1]+1==s_nums[i]:
+                c+=1;
+                print(c)
                 longest=max(longest,c);
-        return longest;
+            else:
+                c=1
+            
+        return longest
+            
+
+        # s=set(nums);
+        # longest=0;
+        # for i in range(len(nums)):
+        #     if nums[i]-1 not in s:
+        #         c=1;
+        #         k=nums[i]+1
+
+        #         while k in s:
+        #             c+=1;
+        #             k+=1
+        #         longest=max(longest,c);
+        # return longest;
 
         
