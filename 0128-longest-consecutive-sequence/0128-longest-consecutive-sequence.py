3d67b2b488ec23b8cfeb168fa06edc6935ac95a4
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-07 17:57:23 -0500

    Time: 9606 ms (5.03%), Space: 26 MB (84.51%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,27 +1,45 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        
         if len(nums)==0:
             return 0
         longest=1;
-        c=1
-        s_nums=sorted(nums);
-        print(s_nums)
-        for i in range(1, len(nums)):
-            start=s_nums[i]
-            if s_nums[i]==s_nums[i-1]:
-                continue;
-            elif s_nums[i-1]+1==s_nums[i]:
-                c+=1;
-                print(c)
-                longest=max(longest,c);
-            else:
-                c=1
+        nums_s=set(nums);
+        print(nums_s)
+        for i in range(len(nums)):
+            if nums[i]-1 not in nums_s:
+                c=1;
+                j=nums[i]+1
+                while j in nums_s:
+                    c+=1;
+                    j+=1
+                    longest=max(longest, c);
+        return longest;
+                    
+                
+
+
+
+
+        # #brute force approach
+        # if len(nums)==0:
+        #     return 0
+        # longest=1;
+        # c=1
+        # s_nums=sorted(nums);
+        # print(s_nums)
+        # for i in range(1, len(nums)):
+        #     start=s_nums[i]
+        #     if s_nums[i]==s_nums[i-1]:
+        #         continue;
+        #     elif s_nums[i-1]+1==s_nums[i]:
+        #         c+=1;
+        #         print(c)
+        #         longest=max(longest,c);
+        #     else:
+        #         c=1
             
-        return longest
+        # return longest
             
 
         # s=set(nums);
