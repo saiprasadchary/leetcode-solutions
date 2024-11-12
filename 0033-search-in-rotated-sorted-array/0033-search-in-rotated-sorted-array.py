563b32a86ffc0802333e80f76cce31509de2d4ea
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-12 18:05:57 -0500

    Time: 0 ms (100%), Space: 11.9 MB (38.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,24 +1,48 @@
 class Solution(object):
     def search(self, nums, target):
    
-        left, right = 0, len(nums) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
+        low=0;
+        n=len(nums);
+        high=n-1;
+        while low<=high:
+            mid=(low+high)//2;
+            if(nums[mid]==target):
+                return mid;
             
-            # Check if the left half is sorted
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target < nums[mid]:  # Target is in the left sorted half
-                    right = mid - 1
-                else:  # Target is in the right half
-                    left = mid + 1
-            else:  # Right half must be sorted
-                if nums[mid] < target <= nums[right]:  # Target is in the right sorted half
-                    left = mid + 1
-                else:  # Target is in the left half
-                    right = mid - 1
+            if nums[low]<=nums[mid]:
+                #left portion is sorted;
+                if nums[low]<=target<nums[mid]:
+                    high=mid-1;
+                else:
+                    low=mid+1;
+            else:
+                #right portion is sorted;
+                if nums[mid]<target<=nums[high]:
+                    low=mid+1; 
+                else:
+                    high=mid-1;
+        return -1;
+        
+        
+        
+        # left, right = 0, len(nums) - 1
+        
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] == target:
+        #         return mid
+            
+        #     # Check if the left half is sorted
+        #     if nums[left] <= nums[mid]:
+        #         if nums[left] <= target < nums[mid]:  # Target is in the left sorted half
+        #             right = mid - 1
+        #         else:  # Target is in the right half
+        #             left = mid + 1
+        #     else:  # Right half must be sorted
+        #         if nums[mid] < target <= nums[right]:  # Target is in the right sorted half
+        #             left = mid + 1
+        #         else:  # Target is in the left half
+        #             right = mid - 1
 
-        return -1  # Target not found
+        # return -1  # Target not found
             
