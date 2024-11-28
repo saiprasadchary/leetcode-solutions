831a0445985f2b87e924981933f6de765e993663
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-27 22:55:26 -0500

    Time: 0 ms (100%), Space: 12.1 MB (12.6%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if x==0:
+            return 0;
+        return x**n
