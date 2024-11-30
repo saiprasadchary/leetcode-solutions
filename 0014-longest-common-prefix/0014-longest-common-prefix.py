c6cd9da365029bd307998edd49fbab1459dead0b
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-30 14:53:09 -0500

    Time: 1 ms (46.53%), Space: 12 MB (9.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,22 +5,23 @@ class Solution(object):
         :rtype: str
         """
         if not strs:
-            return ""
+            return '';
+        if len(strs)==1:
+            return strs[0]
+        str2='';
+        min_length=float("inf")
+        for i in range(len(strs)):
+            if len(strs[i])<min_length:
+                min_length= len(strs[i]);
+        i=0;
+        while i < min_length:
 
-        # Finding the shortest string in the list
-        min_length = min(len(s) for s in strs)
-        
-        lcp = ""
-        for i in range(min_length):
-            # Take the current character from the first string
-            current_char = strs[0][i]
-            
-            # Compare this character with the character at the same position in all other strings
+            k=strs[0][i];
             for s in strs:
-                if s[i] != current_char:
-                    return lcp
-            
-            # If current_char is present at position i in all strings, add it to the result
-            lcp += current_char
-        
-        return lcp
+                if s[i]!=k:
+                    return str2;
+            str2+=k;
+            i+=1
+        return str2;
+
+
