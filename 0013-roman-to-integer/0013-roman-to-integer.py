3e1c777777ae027f46122cfe58056d57f8f10d3d
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-01 16:37:48 -0500

    Time: 11 ms (48.2%), Space: 12 MB (5.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,33 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        normal_case={"I": 1,"V":5,"X":10, "L":50,"C":100, "D":500, "M":1000}
-        special_case = { "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
-        i = 0
-        val = 0
-        while i < len(s):
-            s1=s[i:i+2]
-            if i<len(s)-1:
-                if s1 in special_case:
-                    val+=special_case[s1]
-                    i+=2;
-                    print("s1",val)
-                elif s[i] in normal_case:
-                    val+=normal_case[s[i]]
-                    i+=1;
-                    print(val)
-            else:
-                val+=normal_case[s[i]]
-                i+=1;
-        return val;
-            
+        RomInt={
+        "I":1,"V":5, "X":10, "L":50, "C":100, "D":500, "M":1000};
 
-
-            
-
-
-
-
-            
-
+        SpclCase={"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400,"CM": 900};
         
+        res=0;
+        i=len(s);
+
+        while i!=0:
+            if s[i-2:i] in SpclCase:
+                res+=SpclCase[s[i-2:i]];
+                i-=2;
+            elif s[i-1:i] in RomInt:
+                res+=RomInt[s[i-1:i]];
+                i-=1;
+        return res
