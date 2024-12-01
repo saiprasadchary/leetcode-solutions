385a0320543ea0a52159363ce77673d89d98b8f6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-01 16:38:03 -0500

    [13] [Time Beats: 48.20%] [Memory Beats: 5.20%] - LeetPush

---
+++ 13-Roman-to-Integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def romanToInt(self, s):
+        \\\
+        :type s: str
+        :rtype: int
+        \\\
+        RomInt={
+        \I\:1,\V\:5, \X\:10, \L\:50, \C\:100, \D\:500, \M\:1000};
+
+        SpclCase={\IV\: 4, \IX\: 9, \XL\: 40, \XC\: 90, \CD\: 400,\CM\: 900};
+        
+        res=0;
+        i=len(s);
+
+        while i!=0:
+            if s[i-2:i] in SpclCase:
+                res+=SpclCase[s[i-2:i]];
+                i-=2;
+            elif s[i-1:i] in RomInt:
+                res+=RomInt[s[i-1:i]];
+                i-=1;
+        return res
