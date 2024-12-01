a547783606575b0f7989e129d4e05bc436538922
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-11-30 19:21:02 -0500

    [242] [Time Beats: 47.58%] [Memory Beats: 7.24%] - LeetPush

--- 242-Valid-Anagram.py
+++ 242-Valid-Anagram.py
@@ -6,10 +6,33 @@ class Solution(object):
         :rtype: bool
         \\\
         if len(s)!=len(t):
-            return False
-        s1=sorted(s)
-        t1=sorted(t);
-        if s1==t1:
+            return False;
+        s_t={};
+        
+        for i in s:
+            if i not in s_t:
+                s_t[i]=1;
+            else:
+                s_t[i]+=1;
+        
+        for u,v in zip(s,t):
+            if v in s_t:
+                s_t[v]-=1;
+                if s_t[v]==0:
+                    del[s_t[v]]
+        print(s_t)
+        if len(s_t)==0:
             return True;
         else:
             return False;
+
+
+
+        # if len(s)!=len(t):
+        #     return False
+        # s1=sorted(s)
+        # t1=sorted(t);
+        # if s1==t1:
+        #     return True;
+        # else:
+        #     return False;
