ab939f5bce520ccfcd400efa5a32e777c433fe0c
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-01 13:50:39 -0500

    [1614] [Time Beats: 12.96%] [Memory Beats: 7.80%] - LeetPush

---
+++ 1614-Maximum-Nesting-Depth-of-the-Parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxDepth(self, s):
+        stack=[];
+        k=0;
+        c=0;
+        for i in s:
+            if i==")":
+                c-=1
+                while stack:
+                    if stack.pop()=="(": 
+                        break;
+                    if stack:
+                        stack.pop()
+            elif i=="(":
+                c+=1;
+                k=max(k,c)
+            else: 
+                stack.append(i);
+        return k
+
+        
