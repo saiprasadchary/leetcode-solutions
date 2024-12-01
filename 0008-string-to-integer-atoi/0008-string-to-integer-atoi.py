f04eb2e6babfb65966432b177d5eb79250004016
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-01 17:41:41 -0500

    Time: 0 ms (100%), Space: 12.2 MB (5.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -4,32 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        INT_MAX = 2**31 - 1  # Maximum value for a 32-bit signed integer
-        INT_MIN = -2**31     # Minimum value for a 32-bit signed integer
+        i=0;
+        n=len(s);
+
+        while i<n and s[i]==" ":
+            i+=1;
+        if i==n:
+            return 0;
+
+        sign=1;
+        if i<n and s[i]=="-":
+            sign=-1
+            i+=1
+        elif i<n and s[i]=="+":
+            sign=1
+            i+=1;
+
+        res=0
+        while i<n and s[i].isdigit():
+            res=res*10+int(s[i]);
+            i+=1;
+
+        if sign==1 and res>(2**31-1):
+            res = 2**31-1
+        if sign==-1 and res>(2**31):
+            res = 2**31
         
-        i = 0
-        n = len(s)
-        while i < n and s[i] == ' ':  # Skip leading whitespaces
-            i += 1
-        
-        if i == n:  # If only whitespace is present
-            return 0
-        
-        # Handle sign
-        sign = 1
-        if s[i] == '+' or s[i] == '-':
-            sign = -1 if s[i] == '-' else 1
-            i += 1
-        
-        res = 0
-        while i < n and s[i].isdigit():
-            digit = int(s[i])
-            
-            # Check for overflow and underflow
-            if res > (INT_MAX - digit) / 10:
-                return INT_MIN if sign == -1 else INT_MAX
-            
-            res = res * 10 + digit
-            i += 1
-        
-        return sign * res
+        res=res*sign;
+        return res
