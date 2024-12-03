a88f293945f7e338cae0545098add87f6a7382ea
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-03 16:41:20 -0500

    [151] [Time Beats: 9.02%] [Memory Beats: 7.14%] - LeetPush

--- 151-Reverse-Words-in-a-String.py
+++ 151-Reverse-Words-in-a-String.py
@@ -1,21 +1,26 @@
 class Solution(object):
     def reverseWords(self, s):
-        """
+        \\\
         :type s: str
         :rtype: str
-        """
-        s=s.strip();
-        s2=''
-        s=s.split()
+        \\\
+        s1=s.split();
+        #s1=list(s.split(\ \))
+        print(s1)
 
-        print(s)
-        s=s[::-1]
-        n=len(s)
-        for i in range(n):
-            
-            print(i)
-            s2+=s[i];
-            if i!=n-1:
-                s2+=" "
-        s2.rstrip("  ")
-        return s2;
+        return \ \.join(s1[::-1])
+
+        # s=s.strip();
+        # s2=''
+        # s=s.split()
+
+        # print(s)
+        # s=s[::-1]
+        # n=len(s)
+        # for i in range(n):
+     
+        #     s2+=s[i];
+        #     if i!=n-1:
+        #         s2+=\ \
+        # s2.rstrip(\  \)
+        # return s2;
