c495a0473e5f30fc45aaf9df2b3fca0935206d38
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-08 00:53:05 -0500

    Time: 75 ms (74.66%), Space: 16.1 MB (28.86%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,41 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        if endWord not in wordList:
+            return 0
+        
+        # All words are of the same length.
+        L = len(beginWord)
+        
+        # Dictionary to hold combination of words that can be reached from any given word.
+        # By replacing one letter at a time.
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                all_combo_dict[word[:i] + '_' + word[i+1:]].append(word)
+        
+        # Queue for BFS
+        queue = deque([(beginWord, 1)])
+        # Visited to make sure we don't repeat processing same word.
+        visited = {beginWord}
+        while queue:
+            current_word, level = queue.popleft()    
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + '_' + current_word[i+1:]
+                
+                # Next states are all the words which share the same intermediate state.
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e., the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited.add(word)
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []  # Empty after processing
+        return 0
