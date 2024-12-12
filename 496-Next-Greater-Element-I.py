72eb06c347e1f213f6bed9c9bb7dd52c3b693bf0
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-11 22:56:36 -0500

    [496] [Time Beats: 19.08%] [Memory Beats: 85.92%] - LeetPush

---
+++ 496-Next-Greater-Element-I.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        \\\
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        \\\
+        if nums2:
+            nums2.sort();
+        if nums1:
+            for i in range(len(nums1)):
+                ind=index(nums1[i]);
+
+
+        
+        else:
+            return None;
