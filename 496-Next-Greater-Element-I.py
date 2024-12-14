06c4fef97ef740a48da3770827a7597e9864e5fe
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-13 19:31:35 -0500

    [496] [Time Beats: 22.26%] [Memory Beats: 7.63%] - LeetPush

--- 496-Next-Greater-Element-I.py
+++ 496-Next-Greater-Element-I.py
@@ -1,38 +1,58 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        \\\
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        \\\
-        # n=len(nums1);
-        # m=len(nums2)
-        # while m-1:
-        #     if nums1[n-1] in nums2:
-        #         if stack:
-        #             if stack[-1]>nums
-        #         else:
-        #             res[n-1]=-1;
+        n=len(nums1);
+        m=len(nums2);
+        dictnums1={};
+    
+        for index, val in enumerate(nums1):
+            if val not in dictnums1:
+                dictnums1[val]=index;
+        res=[-1]*n;
 
-
-
-
-
-        
-        l1=[];
-        n=len(nums2);
-        for num in nums1:
-            ind = nums2.index(num)
-            found = False
-           
-            for j in range(ind + 1, n):
-                if nums2[j] > num:
-                    l1.append(nums2[j])
-                    found = True
-                    break
-        
-            if not found:
-                l1.append(-1)
+        for i in range(m):
+            if nums2[i] not in nums1:
+                continue;
+            for j in range(i+1, m):
+                if nums2[j]>nums2[i]:
+                    ind=dictnums1[nums2[i]];
+                    res[ind]=nums2[j];
+                    break;
+        return res;
                 
-        return l1
+
+
+        # stack=[];
+        # ans=[]
+        # for i in range(len(nums2)-1,-1,-1):
+        #     temp=nums2[i]
+        #     if  temp in nums1:
+        #         if stack:
+        #             while stack:
+        #                 if stack[-1]>temp:
+        #                     ans.append(stack[-1]);
+        #                     break
+        #                 else:
+        #                     stack.pop()
+        #         else:
+        #             ans.append(-1)
+        #             stack.append(nums2[i]);
+        #     else:
+        #         stack.append(nums2[i])
+        # return ans;
+        
+        # l1=[];
+        # n=len(nums2);
+        # for num in nums1:
+        #     ind = nums2.index(num)
+        #     found = False
+           
+        #     for j in range(ind + 1, n):
+        #         if nums2[j] > num:
+        #             l1.append(nums2[j])
+        #             found = True
+        #             break
+        #     if not found:
+        #         l1.append(-1)
+                
+        # return l1
         
