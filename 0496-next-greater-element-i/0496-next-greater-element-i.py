7e234a20a5f5101a2ab8d4afad214e0c7de34ba6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-13 20:52:14 -0500

    Time: 0 ms (100%), Space: 12.3 MB (13.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,42 +3,74 @@ class Solution(object):
         n=len(nums1);
         m=len(nums2);
         dictnums1={};
-    
+        stack=[]
+
         for index, val in enumerate(nums1):
             if val not in dictnums1:
                 dictnums1[val]=index;
         res=[-1]*n;
 
-        for i in range(m):
-            if nums2[i] not in nums1:
-                continue;
-            for j in range(i+1, m):
-                if nums2[j]>nums2[i]:
-                    ind=dictnums1[nums2[i]];
-                    res[ind]=nums2[j];
-                    break;
-        return res;
-                
+        for i in range(m-1,-1,-1):
+            curr=nums2[i]
+            while stack and stack[-1]<curr:
+                stack.pop();
+            
+            if curr in dictnums1:
+                if stack:
+                    if stack[-1]>curr:
+                        ind=dictnums1[curr];
+                        res[ind]=stack[-1];
+            stack.append(curr)
+
+        return res
 
 
-        # stack=[];
-        # ans=[]
-        # for i in range(len(nums2)-1,-1,-1):
-        #     temp=nums2[i]
-        #     if  temp in nums1:
-        #         if stack:
-        #             while stack:
-        #                 if stack[-1]>temp:
-        #                     ans.append(stack[-1]);
-        #                     break
-        #                 else:
-        #                     stack.pop()
-        #         else:
-        #             ans.append(-1)
-        #             stack.append(nums2[i]);
-        #     else:
-        #         stack.append(nums2[i])
-        # return ans;
+
+        # dictnums1={};
+    
+        # for index, val in enumerate(nums1):
+        #     if val not in dictnums1:
+        #         dictnums1[val]=index;
+        # res=[-1]*n;
+
+        # for i in range(m):
+        #     if nums2[i] not in nums1:
+        #         continue;
+        #     for j in range(i+1, m):
+        #         if nums2[j]>nums2[i]:
+        #             ind=dictnums1[nums2[i]];
+        #             res[ind]=nums2[j];
+        #             break;
+        # return res;
+        # for index, val in enumerate(nums1):
+        #     if val not in dictnums1:
+        #         dictnums1[val]=index;
+        # res=[-1]*n;
+
+        # for i in range(m):
+        #     if nums2[i] not in nums1:
+        #         continue;
+        #     for j in range(i+1, m):
+        #         if nums2[j]>nums2[i]:
+        #             ind=dictnums1[nums2[i]];
+        #             res[ind]=nums2[j];
+        #             break;
+        # return res;
+        # for index, val in enumerate(nums1):
+        #     if val not in dictnums1:
+        #         dictnums1[val]=index;
+        # res=[-1]*n;
+
+        # for i in range(m):
+        #     if nums2[i] not in nums1:
+        #         continue;
+        #     for j in range(i+1, m):
+        #         if nums2[j]>nums2[i]:
+        #             ind=dictnums1[nums2[i]];
+        #             res[ind]=nums2[j];
+        #             break;
+        # return res;
+ 
         
         # l1=[];
         # n=len(nums2);
