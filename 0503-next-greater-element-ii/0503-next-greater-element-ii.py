aa747c05aeb510825a3aa8006186ec929784e4f5
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-15 03:12:56 -0500

    Time: 43 ms (46.24%), Space: 13.9 MB (23.23%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -6,19 +6,39 @@ class Solution(object):
         """
         n=len(nums);
         res=[-1]*n;
+        stack=[];
 
-        for i in range(n):
-            for j in range(i+1, i+n):
-                ind=j%n;
-                if nums[ind]>nums[i]:
-                    res[i]=nums[ind];
-                    break
-        return res
-            
+        for i in range(2*n-1, -1, -1):
+            ind=i%n
+            if i<n:
+                while stack and stack[-1]<=nums[ind]:
+                    stack.pop();
+
+                if stack and stack[-1]>nums[ind]:
+                    res[ind]=stack[-1];
+                stack.append(nums[ind]);
+            else:
+                while stack and stack[-1]<=nums[ind]:
+                    stack.pop();
+
+                if stack and stack[-1]>nums[ind]:
+                    pass
+                stack.append(nums[ind]);
+        return res;
 
 
+                
 
 
+        ##############
+        # for i in range(n):
+        #     for j in range(i+1, i+n):
+        #         ind=j%n;
+        #         if nums[ind]>nums[i]:
+        #             res[i]=nums[ind];
+        #             break
+        # return res
+        ##############
 
         # n=len(nums)
         # res=[-1]*n
