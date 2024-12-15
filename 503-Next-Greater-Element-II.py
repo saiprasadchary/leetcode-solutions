3cdfb12d206f0d8d6cc43b6a210cb25f4c8ef247
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-15 02:43:39 -0500

    [503] [Time Beats: 5.17%] [Memory Beats: 15.91%] - LeetPush

--- 503-Next-Greater-Element-II.py
+++ 503-Next-Greater-Element-II.py
@@ -4,19 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         \\\
-        n=len(nums)
-        res=[-1]*n
-        
+        n=len(nums);
+        res=[-1]*n;
+
         for i in range(n):
-            found=i
-            for j in range(i+1, n):
-                if nums[j]>nums[i]:
-                    res[i]=nums[j];
-                    found=0
-                    break;
-            if found!=0:
-                for k in range(j):
-                    if nums[k]>nums[i]:
-                        res[i]=nums[k];
-                        break;
-        return res;  
+            for j in range(i+1, i+n):
+                ind=j%n;
+                if nums[ind]>nums[i]:
+                    res[i]=nums[ind];
+                    break
+        return res
+            
+
+
+
+
+
+        # n=len(nums)
+        # res=[-1]*n
+        
+        # for i in range(n):
+        #     found=i
+        #     for j in range(i+1, n):
+        #         if nums[j]>nums[i]:
+        #             res[i]=nums[j];
+        #             found=0
+        #             break;
+        #     if found!=0:
+        #         for k in range(j):
+        #             if nums[k]>nums[i]:
+        #                 res[i]=nums[k];
+        #                 break;
+        # return res;  
