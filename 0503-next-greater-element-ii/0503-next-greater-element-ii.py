62c6ce9e8dd3d8344c60fc05cf9d11c23ffee646
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-15 02:06:44 -0500

    Time: 5578 ms (5.17%), Space: 14.2 MB (15.91%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        res=[-1]*n
+        
+        for i in range(n):
+            found=i
+            for j in range(i+1, n):
+                if nums[j]>nums[i]:
+                    res[i]=nums[j];
+                    found=0
+                    break;
+            if found!=0:
+                for k in range(j):
+                    if nums[k]>nums[i]:
+                        res[i]=nums[k];
+                        break;
+        return res;  
