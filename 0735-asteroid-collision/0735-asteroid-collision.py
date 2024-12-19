6943f091b4d920f59be7256a3c52ab400b946d79
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-12-18 19:55:33 -0500

    Time: 12 ms (28.16%), Space: 13.4 MB (12.32%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        st=[];
+        
+        for i in range(len(asteroids)):
+            if asteroids[i]>0:
+                st.append(asteroids[i])
+            else:
+                while(st and st[-1]>0 and abs(asteroids[i])>st[-1]):
+                    st.pop();
+                if(st and st[-1] == abs(asteroids[i])):
+                    st.pop()
+                elif( st and st[-1]<0):
+                    st.append(asteroids[i]);
+                elif (not st or (st and st[-1]<0)):
+                    st.append(asteroids[i]);
+        return (st)
