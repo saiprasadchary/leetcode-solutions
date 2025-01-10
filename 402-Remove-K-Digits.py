a5410b04c9255571f3651ef505103682bdf6026a
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-01-09 21:04:45 -0600

    [402] [Time Beats: 94.00%] [Memory Beats: 12.00%] - LeetPush

---
+++ 402-Remove-K-Digits.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        \\\
+        :type num: str
+        :type k: int
+        :rtype: str
+        \\\
+        st = []
+        n = len(num)
+        if n == k:
+            return \0\
+
+        for i in range(n):
+            while st and k > 0 and st[-1] > num[i]:
+                st.pop()
+                k -= 1
+            st.append(num[i])
+        
+        # If there are still k digits left to remove, remove them from the end
+        while k > 0:
+            st.pop()
+            k -= 1
+        
+        # Convert stack to string and strip leading zeros
+        result = ''.join(st).lstrip('0')
+        return result if result else \0\
+
+
+        
