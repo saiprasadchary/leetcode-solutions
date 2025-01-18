93d7204ac54171452d0408c7198d354415676e7f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-01-17 22:31:22 -0600

    [3] [Time Beats: 94.12%] [Memory Beats: 60.03%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        \\\
+        :type s: str
+        :rtype: int
+        \\\
+        seen = {}
+        left = 0
+        max_size = 0
+
+        for right, char in enumerate(s):
+            if char not in seen:
+                max_size = max(max_size, right - left + 1)
+            
+            else: 
+                # not in the window so we can keep going
+                if seen[char] < left:
+                    max_size = max(max_size, right - left + 1)
+                else: 
+                # we need to reset left to the seen pointer
+                    left = seen[char] + 1
+
+            # Storing at which index we found the character
+            seen[char] = right
+
+        return max_size
