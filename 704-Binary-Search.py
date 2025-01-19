6a9fac5e9a19ee4bc39dd4dfa591c1307795f58f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-01-18 21:26:00 -0600

    [704] [Time Beats: 5.57%] [Memory Beats: 16.92%] - LeetPush

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -5,19 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        n=len(nums);
-        if n<=0:
-            return -1;
-        left=0;
-        right=n-1;
+        left, right = 0, len(nums) - 1
         
-        while left<=right:
-            mid =left+right//n;
-            if nums[mid]==target:
-                return mid;
-            elif target> nums[mid]:
-                left=mid+1;
+        while left <= right:
+            mid = left + (right - left) 
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                right=mid-1;
-        return -1     
+                right = mid - 1
+        
+        return -1  
+
+
+solution = Solution()
+print(solution.search([-1, 0, 3, 5, 9, 12], 9))  
+print(solution.search([-1, 0, 3, 5, 9, 12], 2))  
         
