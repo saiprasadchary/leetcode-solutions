c73135324f0412b672b7dd691076cea44d9723c3
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-01-21 17:30:32 -0600

    Time: 0 ms (100%), Space: 12.5 MB (6.95%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,22 +1,11 @@
 class Solution(object):
-    def AlreadySorted(self,nums):
-        for i in range(1,len(nums)):
-            if nums[i]<nums[i-1]:
-                print(i)
-                return i;
-        return True
-        
     def check(self, nums):
-
-        partitionIndex=self.AlreadySorted(nums)
-        print(partitionIndex)
-        if partitionIndex==True and type(partitionIndex)==bool:
-            return True;
+        count=0
+        n=len(nums)
+        for i in range(len(nums)):
+            if nums[i]>nums[(i+1)%n]:
+                count=count+1
+        if count>1:
+            return False
         else:
-            arr=nums[partitionIndex:]+nums[0:partitionIndex]
-            partitionIndex=self.AlreadySorted(arr)
-            if (self.AlreadySorted(arr)==True) and type(partitionIndex)==bool:
-                print("check")
-                return True;
-            else:
-                return False
+            return True
