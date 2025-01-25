136152dc97f8fd5a88ad4a5a2e3007803f8589aa
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-01-24 21:09:25 -0600

    [75] [Time Beats: 100.00%] [Memory Beats: 36.61%] - LeetPush

---
+++ 75-Sort-Colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        \\\
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        \\\
+        count = [0, 0, 0]
+        for i in range(len(nums)):
+            count[nums[i]] = count[nums[i]] + 1
+        
+        i = 0
+        for color in range(len(count)):
+            for _ in range(count[color]):
+                nums[i] = color
+                i += 1
