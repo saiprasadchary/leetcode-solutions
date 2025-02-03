143e88d5534255da27e906aa7775e4ce3b6ed3e2
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-02-02 22:55:19 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,49 +4,4 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices)<2:
-            return 0;
-        profit=0;
-
-        mini=prices[0];
-        for i in range(1, len(prices)):
-            if prices[i]-mini>0:
-                profit=max(profit, prices[i]-mini)
-            else:
-                mini=min(mini, prices[i]);
-        return profit;
-
-
-#         # profit=0;
-#         # for i in range(len(prices)):
-#         #     for j in range(i+1, len(prices)):
-#         #         if prices[i]< prices[j]:
-#         #             temp=prices[j]-prices[i];
-#         #             profit=max(temp, profit);
-
-#         # return profit;
-
-
-#         profit=0;
-#         smallValInd=-1;
-
-#         mini=float("inf")
-#         for i in range(len(prices)-1):
-#             if prices[i]<mini:
-#                 mini=prices[i];
-#                 smallValInd=i;
-#                 print(mini, smallValInd)
-#         print(mini, smallValInd)
-
-#         for i in range(smallValInd, len(prices)):
-#             if prices[i]-mini > 0:
-#                 temp=prices[i]-mini;
-#                 profit=max(profit, temp);
-#         return profit;
-
-
-
-
-        
-        
         
