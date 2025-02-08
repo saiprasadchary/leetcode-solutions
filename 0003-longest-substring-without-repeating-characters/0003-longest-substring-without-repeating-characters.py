f5e9c74ef99d1e9a79322f104b15afcebca8c168
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-02-08 14:56:17 -0600

    Time: 20 ms (72.85%), Space: 12.6 MB (69.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,15 +5,41 @@ class Solution(object):
         :rtype: int
         """
         n=len(s)
-        maxlen=0;
-        for i in range(n):
-            l1=[];
-            for j in range(i, n):
-                if s[j] in l1:
-                    break;
-                len1=j-i+1;
-                maxlen=max(maxlen,len1)
-                l1.append(s[j])
-        return maxlen;
+        l=0
+        r=0
+        dict1={}
+        maxlen=0
+        while r<n:
+            
+            if s[r] in dict1:
+                if dict1[s[r]]>=l:
+                    l=dict1[s[r]]+1
+            len1=r-l+1;
+            maxlen=max(len1, maxlen)
+            dict1[s[r]]=r
+            r+=1
+        return maxlen
+
+
+
+
+
+
+
+
+
+
+
+        # n=len(s)
+        # maxlen=0;
+        # for i in range(n):
+        #     l1=[];
+        #     for j in range(i, n):
+        #         if s[j] in l1:
+        #             break;
+        #         len1=j-i+1;
+        #         maxlen=max(maxlen,len1)
+        #         l1.append(s[j])
+        # return maxlen;
 
         
