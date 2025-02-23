2537f28bd5e4e45786c93553d98b27b4c81df12d
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-02-23 16:49:28 -0600

    Time: 12 ms (95.12%), Space: 12.6 MB (70.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,37 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         n=len(s)
-        l=0
-        r=0
-        dict1={}
+        l=r=0
         maxlen=0
+        #to check the window duplication we need dictionary
+        dict_wind={}
+        # it is a two pointer technique to manipulate the wind_size using the dictionary 
         while r<n:
-            if s[r] in dict1:
-                if dict1[s[r]]>=l:
-                    l=dict1[s[r]]+1
-            len1=r-l+1;
-            maxlen=max(len1, maxlen)
-            dict1[s[r]]=r
+            # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
+            # unnecessary values to remove the duplicates in the dictionary
+            if s[r] in dict_wind:
+                if dict_wind[s[r]]>=l:
+                    l=dict_wind[s[r]]+1 # cause as it skips the unnecessary traversal to skip, the pointer l 
+                #l should point to the next value to make it sensible(without duplication)
+
+            # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
+            # unnecessary values to remove the duplicates in the dictionary
+            maxlen=max(r-l+1, maxlen)
+            dict_wind[s[r]]=r
             r+=1
         return maxlen
 
-
-
-
-
-
-
-
-
+        # dict1={}
+        # maxlen=0
+        # while r<n:
+        #     if s[r] in dict1: 
+        #         if dict1[s[r]]>=l:
+        #             l=dict1[s[r]]+1
+        #     len1=r-l+1;
+        #     maxlen=max(len1, maxlen)
+        #     dict1[s[r]]=r
+        #     r+=1
+        # return maxlen
 
 
         # n=len(s)
