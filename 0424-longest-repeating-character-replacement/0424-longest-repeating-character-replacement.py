8537e5c6b046e45d66bebfa96f136e1d290a3150
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-02-25 22:19:53 -0600

    Time: 160 ms (51.93%), Space: 12.6 MB (95.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -13,7 +13,9 @@ class Solution:
         n=len(s)
         while r<n:
             freq[s[r]]=freq.get(s[r],0)+1
-            max_freq=max(max_freq, freq[s[r]])
+           # max_freq=max(max_freq, freq[s[r]])
+            max_freq=max(freq.values())
+
             wind=r-l+1
             if wind-max_freq<=k:
                 longest=max(longest,wind)
