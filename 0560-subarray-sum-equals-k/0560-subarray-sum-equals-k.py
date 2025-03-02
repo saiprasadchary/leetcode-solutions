7c4b35af22f4e96569b926d308da0b3c09b40962
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-01 22:14:49 -0600

    Time: 43 ms (41.58%), Space: 15.4 MB (21.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,25 +1,43 @@
 class Solution(object):
     def subarraySum(self, nums, k):
+        # n=len(nums)
+        # count=0
+        # pfs=[0]*n
+        # pfs[0]=nums[0]
+        # for i in range(1,n):
+        #     pfs[i]+=pfs[i-1]+nums[i]
+
+        # m={}
+        # for j in range(n):
+        #     if pfs[j]==k:
+        #         count +=1
+        #     key=pfs[j]-k
+        #     if key in m:
+        #         count += m[key]
+        #     if pfs[j] in m:
+        #         m[pfs[j]] += 1
+        #     else:
+        #         m[pfs[j]] = 1
+            
+        # return count
+
+        #####
         n=len(nums)
-        count=0
-        pfs=[0]*n
-        pfs[0]=nums[0]
-        for i in range(1,n):
-            pfs[i]+=pfs[i-1]+nums[i]
+        freq={}
+        freq[0]=1
+        pref_sum=0
+        cnt=0
+        for i in range(n):
+            pref_sum += nums[i]
+            if(pref_sum-k in freq):
+                cnt+=freq[pref_sum-k]
+            freq[pref_sum] = freq.get(pref_sum, 0) + 1
 
-        m={}
-        for j in range(n):
-            if pfs[j]==k:
-                count +=1
-            key=pfs[j]-k
-            if key in m:
-                count += m[key]
-            if pfs[j] in m:
-                m[pfs[j]] += 1
-            else:
-                m[pfs[j]] = 1
+        return cnt
 
-        return count
+            
+
+
 
 
         # count=0
