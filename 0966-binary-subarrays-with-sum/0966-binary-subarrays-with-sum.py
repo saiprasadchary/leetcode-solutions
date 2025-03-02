69f9273173adae6e88671d21980fc7865e3b6ba3
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-02 00:17:24 -0600

    Time: 75 ms (5.05%), Space: 12.8 MB (88.03%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -5,16 +5,36 @@ class Solution(object):
         :type goal: int
         :rtype: int
         """
-        cnt=0
-        freq={0:1}
+        return self.BSS(nums,goal)- self.BSS(nums, goal-1)
+
+    def BSS(self, nums,goal):
+        l=r=0
         n=len(nums)
-        pref_sum=0
-        for i in range(n):
-            pref_sum+=nums[i]
-            if pref_sum-goal in freq:
-                cnt+=freq[pref_sum-goal]
-            freq[pref_sum]=freq.get(pref_sum,0)+1
-        return cnt;
+        cnt=0
+        sum1=0
+        if goal<0:
+            return 0
+        while r<n:
+            sum1+=nums[r]
+            while sum1>goal:
+                sum1-=nums[l]
+                l+=1
+            if sum1<=goal:
+                cnt+=r-l+1
+            r+=1
+        return cnt
+
+        # this solution could solve for all the values irrespective of just 0's and 1's and also +ve and -ve's too
+        # cnt=0
+        # freq={0:1}
+        # n=len(nums)
+        # pref_sum=0
+        # for i in range(n):
+        #     pref_sum+=nums[i]
+        #     if pref_sum-goal in freq:
+        #         cnt+=freq[pref_sum-goal]
+        #     freq[pref_sum]=freq.get(pref_sum,0)+1
+        # return cnt;
 
             
 
