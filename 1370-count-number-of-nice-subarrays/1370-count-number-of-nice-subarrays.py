c3dd3ce04eb5fb2bfba0b4e01091387a5bac389c
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-02 14:39:22 -0600

    Time: 202 ms (9.14%), Space: 16.3 MB (69.81%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,70 +1,69 @@
 class Solution(object):
     def numberOfSubarrays(self, nums, k):
-    #     n = len(nums)
-    #     count = 0
-    #     binary_nums=[0]*n
+        n = len(nums)
+        count = 0
+        binary_nums=[0]*n
 
-    #     for i in range(n):
-    #         if nums[i]%2!=0:
-    #             binary_nums[i]=1
-    #     #after transformation the problem becomes similar to binary Subarrays with sum
-    #     return self.BBS(binary_nums, k) - self.BBS(binary_nums, k-1)
-
-    # def BBS(self, binary_nums,goal):
-    #     l=r=0
-    #     n=len(binary_nums)
-    #     pref_sum=0
-    #     cnt=0
-    #     if goal<0:
-    #         return 0
-    #     while r<n:
-    #         pref_sum+=binary_nums[r]
-    #         while pref_sum>goal:
-    #             pref_sum-=binary_nums[l]
-    #             l+=1
-    #         if pref_sum<=goal:
-    #             cnt+=r-l+1
-    #         r+=1
-    #     print(cnt)
-    #     return cnt
-
-
-
-        n=len(nums)
-        count=0
-        # Transform the array: odd numbers become 1, even numbers become 0.
-        binary = [0] * n
         for i in range(n):
-            if nums[i] % 2 == 0:
-                binary[i] = 0
-            else:
-                binary[i] = 1  # Instead of nums[i], we want 1 for odd numbers.
+            if nums[i]%2!=0:
+                binary_nums[i]=1
+        #after transformation the problem becomes similar to binary Subarrays with sum
+        return self.BBS(binary_nums, k) - self.BBS(binary_nums, k-1)
 
-        # Build the prefix sum array.
-        pf = [0] * n
-        pf[0] = binary[0]
-        for i in range(1, n):
-            pf[i] = pf[i - 1] + binary[i]
+    def BBS(self, binary_nums,goal):
+        l=r=0
+        n=len(binary_nums)
+        pref_sum=0
+        cnt=0
+        if goal<0:
+            return 0
+        while r<n:
+            pref_sum+=binary_nums[r]
+            while pref_sum>goal:
+                pref_sum-=binary_nums[l]
+                l+=1
+            if pref_sum<=goal:
+                cnt+=r-l+1
+            r+=1
+        print(cnt)
+        return cnt
 
-        # Dictionary to store frequency of prefix sums.
-        dict1 = {}
-        for j in range(n):
-            # If the current prefix sum equals k, the subarray from index 0 to j is valid.
-            if pf[j] == k:
-                count += 1
 
-            # Find the target prefix sum that would give a subarray sum of k.
-            key = pf[j] - k
-            if key in dict1:
-                count += dict1[key]
 
-            # Update the dictionary for the current prefix sum.
-            if pf[j] in dict1:
-                dict1[pf[j]] += 1
-            else:
-                dict1[pf[j]] = 1
 
-        return count
+        # # Transform the array: odd numbers become 1, even numbers become 0.
+        # binary = [0] * n
+        # for i in range(n):
+        #     if nums[i] % 2 == 0:
+        #         binary[i] = 0
+        #     else:
+        #         binary[i] = 1  # Instead of nums[i], we want 1 for odd numbers.
+
+        # # Build the prefix sum array.
+        # pf = [0] * n
+        # pf[0] = binary[0]
+        # for i in range(1, n):
+        #     pf[i] = pf[i - 1] + binary[i]
+
+        # # Dictionary to store frequency of prefix sums.
+        # dict1 = {}
+        # for j in range(n):
+        #     # If the current prefix sum equals k, the subarray from index 0 to j is valid.
+        #     if pf[j] == k:
+        #         count += 1
+
+        #     # Find the target prefix sum that would give a subarray sum of k.
+        #     key = pf[j] - k
+        #     if key in dict1:
+        #         count += dict1[key]
+
+        #     # Update the dictionary for the current prefix sum.
+        #     if pf[j] in dict1:
+        #         dict1[pf[j]] += 1
+        #     else:
+        #         dict1[pf[j]] = 1
+
+        # return count
 
 
 
