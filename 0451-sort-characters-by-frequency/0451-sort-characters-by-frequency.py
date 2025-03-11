7b1afa17ecb9679d2b9c29aa8bd2ea292804707b
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-11 14:14:00 -0500

    Time: 14 ms (83.91%), Space: 13.5 MB (53.46%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -4,38 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        freq={}
-        #heap implementation
-        for i in range(len(s)):
-            freq[s[i]]=freq.get(s[i],0)+1
-        heap=[]
-        for char, frequ in freq.items():
-            heapq.heappush(heap, (-frequ, char))
-        res=""
-        while heap:
-            freq_val, character = heapq.heappop(heap)
-            print(-freq_val, character)
-            z=-freq_val
-            for j in range(z):
-                res+=(character)
-        return res
-
-
-        # dict1={};
-        # for i in s:
-        #     if i not in dict1:
-        #         dict1[i]=1;
-        #     else:
-        #         dict1[i]+=1;
-
-        # dict2=(sorted(dict1.items(), key = lambda x: x[1], reverse = True))
+        # freq={}
+        # #heap implementation
+        # for i in range(len(s)):
+        #     freq[s[i]]=freq.get(s[i],0)+1
+        # heap=[]
+        # for char, frequ in freq.items():
+        #     heapq.heappush(heap, (-frequ, char))
         # res=""
-        # print((dict2))
-        
-        # for char, freq in dict2:
-        #     res += char * freq  # Correctly repeat char 'freq' times
-
+        # while heap:
+        #     freq_val, character = heapq.heappop(heap)
+        #     print(-freq_val, character)
+        #     z=-freq_val
+        #     for j in range(z):
+        #         res+=(character)
         # return res
+
+
+        dict1={};
+        for i in s:
+            if i not in dict1:
+                dict1[i]=1;
+            else:
+                dict1[i]+=1;
+
+        dict2=(sorted(dict1.items(), key = lambda x: x[1], reverse = True))
+        res=""
+        print((dict2))
+        
+        for char, freq in dict2:
+            res += char * freq  # Correctly repeat char 'freq' times
+
+        return res
         
             
         
