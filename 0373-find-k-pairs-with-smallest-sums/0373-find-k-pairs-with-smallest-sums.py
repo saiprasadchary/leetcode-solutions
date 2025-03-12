3cdda98b8cbe89c73e3ab8a649d0d75739294436
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-12 17:39:29 -0500

    Time: 154 ms (71.63%), Space: 27.8 MB (17.91%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -13,21 +13,29 @@ class Solution(object):
         i=j=0
         heapq.heappush(heap, ((nums1[i]+nums2[j]), i,j))
         visited.add((i,j))
-        #k-=1
         res=[]
+        #The heap and set are initialized based on which the while loop works as heap is not empty and visited to track any redundant
+
+
         while k>0 and heap:
             _,i,j=heapq.heappop(heap)
             res.append([nums1[i], nums2[j]])
             
+            #neighbour checks by incrementing (i+1,j) and (i,j+1) to push them into the minheap to extract the minimum of existing and newly pushed values 
             if(i+1<n and (i+1, j) not in visited):
                 heapq.heappush(heap, ((nums1[i+1]+nums2[j]), i+1, j))
+                # redundancy checks
                 visited.add((i+1,j))
             
             if(j+1<m and (i,j+1) not in visited):
                 heapq.heappush(heap, ((nums1[i]+nums2[j+1]), i, j+1))
                 visited.add((i,j+1))
+            # everytime we get a oair we decrement the k val
             k-=1
         return res
+
+
+        
         # heap=[]
         # res=[]
         # for i in range(len(nums1)):
