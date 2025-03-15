d9f23022e73e79899f6ba08baba9ff98a0ed7dc9
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-15 15:24:05 -0500

    Time: 0 ms (100%), Space: 12.5 MB (15.31%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # we maintain a range of min and max values to check the count
+        mini,maxi=0,0
+
+        for i in range(len(s)):
+            if(s[i]=="("):
+                mini+=1
+                maxi+=1
+            elif(s[i]==")"):
+                mini-=1
+                maxi-=1
+            else:
+                mini-=1
+                maxi+=1
+            if(mini<0 and maxi>=0):
+                mini=0
+            if(maxi<0):
+                return False
+        return (mini==0)
+
+        
