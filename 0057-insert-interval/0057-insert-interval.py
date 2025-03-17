9904a5b92eedbc367aaf492c691e10b95a9e1255
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-17 15:52:09 -0500

    Time: 55 ms (5.27%), Space: 14.2 MB (48.32%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        #we need to run the pointer to the point where the merging happens
+        
+        i=0
+        res=[]
+        n=len(intervals)
+
+        # left part to append the safe intervals in the res array - traversing
+        while i<n and intervals[i][1]<newInterval[0]:
+
+            res.append(intervals[i])
+            i+=1
+        
+        # Now overlaping part here the overlapping is alreadily defined by above condition check
+            # all we need to do is to find where the second value in the new interval and the first value in the current array are <=
+             #consider interval [3, 3] and newInterval [3, 3] 
+        while i< n and intervals[i][0]<=newInterval[1]:
+            newInterval[0]=min(newInterval[0], intervals[i][0])
+            newInterval[1]=max(newInterval[1], intervals[i][1])
+            
+            print(res)
+            i+=1
+        res.append(newInterval)
+
+        # right most part 
+
+        while i<n:
+            res.append(intervals[i])
+            i+=1
+        print(res)
+        return res
+        
