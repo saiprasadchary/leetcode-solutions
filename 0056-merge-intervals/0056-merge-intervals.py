424857a09dc98d78ff6794981141c3b7623da786
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-17 23:15:53 -0500

    Time: 15 ms (25.57%), Space: 15.9 MB (83.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,31 +1,33 @@
 class Solution:
     def merge(self, intervals):
+
         # Sort intervals by their start time
         intervals.sort(key=lambda x: x[0])
-        
-        res = []
-        n = len(intervals)
-        
-        # If no intervals, return empty
-        if n == 0:
-            return res
-        
-        # Initialize 'temp' to the first interval
-        temp = [intervals[0][0], intervals[0][1]]
-        i = 1  # we'll process from the second interval onward
-        
-        # Single while loop to merge
-        while i < n:
-            # If intervals[i] overlaps with 'temp'
-            if intervals[i][0] <= temp[1]:
-                # Merge by updating the end
-                temp[1] = max(temp[1], intervals[i][1])
+
+        res=[]
+        i=0
+        n=len(intervals)
+        temp=intervals[0]
+
+        #we just need to check if there is a overlapping or not
+        #consider the initial values as temp used for comparisions
+         #_________Overlap comparision is checked as if the temp is to the roght of the current arr values and arr[i][0]<=temp[i]__
+
+        while i< n:
+
+            if(intervals[i][0]<=temp[1]):
+                temp[0]=min(intervals[i][0], temp[0])
+                temp[1]=max(intervals[i][1], temp[1])
+
             else:
-                # No overlap, push the current 'temp' to 'res' and reset
                 res.append(temp)
-                temp = [intervals[i][0], intervals[i][1]]
-            i += 1
-        
-        # After loop, append the last 'temp'
+                temp=intervals[i]
+            i+=1
         res.append(temp)
+
         return res
+
+
+
+        
+        
