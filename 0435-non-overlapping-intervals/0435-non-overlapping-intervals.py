c54bb1170e9f32ae6473b342734e557a0c0d4130
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-18 18:49:39 -0500

    Time: 139 ms (74.86%), Space: 45.2 MB (59.97%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -5,7 +5,10 @@ class Solution(object):
         :rtype: int
         """
         n=len(intervals)
+        if n==1:
+            return 0
         cnt=1
+
         
         intervals.sort(key=lambda x:x[1])
         lastInd=intervals[0][1]
@@ -14,6 +17,5 @@ class Solution(object):
             if(intervals[i][0]>=lastInd):
                 cnt+=1
                 lastInd=intervals[i][1]
-        print(intervals)
 
         return n-cnt
