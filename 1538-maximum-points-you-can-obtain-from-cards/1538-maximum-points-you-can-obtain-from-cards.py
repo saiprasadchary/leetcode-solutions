8b0583c8e54e50313d1f32715b590794620f66dd
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-18 19:44:11 -0500

    Time: 23 ms (75.41%), Space: 19.4 MB (58.39%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -6,16 +6,26 @@ class Solution(object):
         :rtype: int
         """
         n=len(cardPoints)
+
+        # initial consideration of sum could be initiated to the l_sum  [_______]____________ , _____]_____[_ .. so on therefore forloop starts in reverse from k
         l_sum=sum(cardPoints[:k])
         r_sum=0
+        # this palys a pointer role to consider from end to the right_sum to K
         r_index=n-1
         max_sum=l_sum
+
         for i in range(k-1,-1,-1):
+
+            # removal from left_Sum
             l_sum-=cardPoints[i]
+
+            # adding into the right_sum
             r_sum+=cardPoints[r_index]
+            
             r_index-=1
             
             max_sum=max(max_sum, l_sum+r_sum)
+
         return max_sum
 
             
