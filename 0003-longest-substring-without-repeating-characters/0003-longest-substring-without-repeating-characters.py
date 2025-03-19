7fdea737ca30766da41e8ce7c4aa5d8555ed66e2
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-19 12:14:26 -0500

    Time: 34 ms (28.6%), Space: 12.3 MB (99.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,25 +1,25 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        n=len(s)
-        l=r=0
-        maxlen=0
-        #to check the window duplication we need dictionary
-        dict_wind={}
-        # it is a two pointer technique to manipulate the wind_size using the dictionary 
-        while r<n:
-            # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
-            # unnecessary values to remove the duplicates in the dictionary
-            if s[r] in dict_wind:
-                if dict_wind[s[r]]>=l: #this check is to make sure the traversal i.e by moving the l pointer to he forward not backwards
-                    l=dict_wind[s[r]]+1 # cause as it skips the unnecessary traversal to skip, the pointer l 
-                #l should point to the next value to make it sensible(without duplication)
+        # n=len(s)
+        # l=r=0
+        # maxlen=0
+        # #to check the window duplication we need dictionary
+        # dict_wind={}
+        # # it is a two pointer technique to manipulate the wind_size using the dictionary 
+        # while r<n:
+        #     # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
+        #     # unnecessary values to remove the duplicates in the dictionary
+        #     if s[r] in dict_wind:
+        #         if dict_wind[s[r]]>=l: #this check is to make sure the traversal i.e by moving the l pointer to he forward not backwards
+        #             l=dict_wind[s[r]]+1 # cause as it skips the unnecessary traversal to skip, the pointer l 
+        #         #l should point to the next value to make it sensible(without duplication)
 
-            # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
-            # unnecessary values to remove the duplicates in the dictionary
-            maxlen=max(r-l+1, maxlen)
-            dict_wind[s[r]]=r
-            r+=1
-        return maxlen
+        #     # the pointer r moves through out the last of the string whereas the l poiner points by skipping the
+        #     # unnecessary values to remove the duplicates in the dictionary
+        #     maxlen=max(r-l+1, maxlen)
+        #     dict_wind[s[r]]=r
+        #     r+=1
+        # return maxlen
 
         # dict1={}
         # maxlen=0
@@ -45,5 +45,21 @@ class Solution(object):
         #         maxlen=max(maxlen,len1)
         #         l1.append(s[j])
         # return maxlen;
+        nums=s
+        n=len(nums)
+        l=r=0
+        maxWind=0
+        freq={}
+        while r<n:
+            freq[nums[r]]=freq.get(nums[r],0)+1
+            while(freq[nums[r]]>1):
+                
+                freq[nums[l]]-=1
+                l+=1
+            #if(len(freq)==r-l+1):
+            maxWind=max(maxWind, r-l+1)
+            r+=1
+        return maxWind
+
 
         
