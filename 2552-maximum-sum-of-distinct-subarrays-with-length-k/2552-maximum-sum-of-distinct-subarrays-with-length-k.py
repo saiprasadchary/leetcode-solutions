d9f3f55ac6031396992efd5232e863ec72807c56
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-18 21:42:14 -0500

    Time: 157 ms (40.06%), Space: 26.9 MB (17.9%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,28 +1,52 @@
 class Solution(object):
     def maximumSubarraySum(self, nums, k):
+        # n=len(nums)
+        # dict_wind={}
+        # wind_sum=0
+        # maxSum=0
+        # for i in range(n):
+        #     #considering every values into the dictionary
+        #     if nums[i] not in dict_wind:
+        #         dict_wind[nums[i]]=1
+        #         wind_sum+=nums[i]
+        #     else:
+        #         dict_wind[nums[i]]+=1
+        #         wind_sum+=nums[i]
+        #     #incrementing the sum of the window irrespective of its presence in dictionary
+
+        #     if i>=k:
+        #         #decrementing the values to be deleted in the dict in the wind-sum
+        #         wind_sum-=nums[i-k]
+        #         dict_wind[nums[i-k]]-=1
+        #         #removing its values 0 too, to clear the dictionary of unnecessary keys
+        #         if dict_wind[nums[i-k]]==0:
+        #             del dict_wind[nums[i-k]]
+        #     if len(dict_wind)==k:
+        #         maxSum=max(maxSum, wind_sum)
+        # return maxSum
+
+
+
         n=len(nums)
-        dict_wind={}
+        freq={}
+        left=0
         wind_sum=0
         maxSum=0
-        for i in range(n):
-            #considering every values into the dictionary
-            if nums[i] not in dict_wind:
-                dict_wind[nums[i]]=1
-                wind_sum+=nums[i]
-            else:
-                dict_wind[nums[i]]+=1
-                wind_sum+=nums[i]
-            #incrementing the sum of the window irrespective of its presence in dictionary
 
-            if i>=k:
-                #decrementing the values to be deleted in the dict in the wind-sum
-                wind_sum-=nums[i-k]
-                dict_wind[nums[i-k]]-=1
-                #removing its values 0 too, to clear the dictionary of unnecessary keys
-                if dict_wind[nums[i-k]]==0:
-                    del dict_wind[nums[i-k]]
-            if len(dict_wind)==k:
+        for i in range(n):
+            freq[nums[i]]=freq.get(nums[i],0)+1
+            wind_sum+=nums[i]
+
+            if(i>=k):
+                wind_sum-=nums[left]
+                freq[nums[left]]-=1
+                if(freq[nums[left]]==0):
+                    del freq[nums[left]]
+                left+=1
+
+            if(len(freq)==k):
                 maxSum=max(maxSum, wind_sum)
+                
         return maxSum
 
 
@@ -49,7 +73,7 @@ class Solution(object):
         # return res
 
 
-
+#below are brute force approaches
 
         # res=0
         # for i in range(n):
@@ -67,4 +91,25 @@ class Solution(object):
         #             res=max(res,sum1)
         #             break
         # return res
+
+
+        # n=len(nums)
+        # maxSum=0
+        # for i in range(n):
+
+        #     visited=set()
+        #     sum1=0
+
+        #     for j in range(i, n):
+
+        #         if(nums[j] in visited):
+        #             break
+        #         else:   
+        #             sum1+=nums[j]
+        #             if(j-i+1==k):
+        #                 maxSum=max(maxSum, sum1)
+        #                 break
+        #             visited.add(nums[j])
+
+        # return maxSum
         
