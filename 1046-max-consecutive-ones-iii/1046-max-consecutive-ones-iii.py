148accaa6ec3ab390b0553345d7ac53081ff1d72
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-20 22:16:03 -0500

    Time: 65 ms (95.54%), Space: 12.8 MB (88.49%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -5,47 +5,47 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # l=r=0
-        # n=len(nums)
-        # zeros=0
-        # maxlen=0
-        # while r<n:
-        #     if nums[r]==0:
-        #         zeros+=1
-        #     # here the twist comes until we find the zeros condition to be true we keep the windsize to shrink by one through the if statement and simplify doesnt considers the maxlen until zeros condition is satisfied.
-        #     if zeros > k:
-        #         if nums[l]==0:
-        #             zeros-=1
-        #         l+=1
-        #     if zeros <=k:
-        #         windlen=r-l+1
-        #         maxlen=max(windlen, maxlen)
-        #     r+=1
-        # return maxlen
+        l=r=0
+        n=len(nums)
+        zeros=0
+        maxlen=0
+        while r<n:
+            if nums[r]==0:
+                zeros+=1
+            # here the twist comes until we find the zeros condition to be true we keep the windsize to shrink by one through the if statement and simplify doesnt considers the maxlen until zeros condition is satisfied.
+            if zeros > k:
+                if nums[l]==0:
+                    zeros-=1
+                l+=1
+            if zeros <=k:
+                windlen=r-l+1
+                maxlen=max(windlen, maxlen)
+            r+=1
+        return maxlen
 
 
 
 
         
         # this is the better solution actually as while loop for the l pointer, until it finds the another zero is shouldbe eliminated in the optimal one which is shown above:
-        n=len(nums)
-        r=0
-        l=0
-        zeros=0
-        maxlen=0
-        while r<n:
-            if nums[r]==0:
-                zeros+=1;
-            if zeros<=k:
-                len1=r-l+1
-                maxlen=max(len1, maxlen)
-            if zeros>k:
-                while zeros>k:
-                    if nums[l]==0:
-                        zeros-=1
-                    l+=1
-            r+=1
-        return maxlen
+        # n=len(nums)
+        # r=0
+        # l=0
+        # zeros=0
+        # maxlen=0
+        # while r<n:
+        #     if nums[r]==0:
+        #         zeros+=1;
+        #     if zeros<=k:
+        #         len1=r-l+1
+        #         maxlen=max(len1, maxlen)
+        #     if zeros>k:
+        #         while zeros>k:
+        #             if nums[l]==0:
+        #                 zeros-=1
+        #             l+=1
+        #     r+=1
+        # return maxlen
 
 
         # maxlen=0
