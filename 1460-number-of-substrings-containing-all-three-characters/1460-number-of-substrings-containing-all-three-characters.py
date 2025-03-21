302a2eacbc8d0ce03b7d4357a32f6ad7a7488def
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-21 12:44:51 -0500

    Time: 221 ms (31.88%), Space: 12.6 MB (81.51%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -4,18 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        n=len(s)
-        cnt=0
-        last_seen={'a':-1, 'b':-1, 'c':-1}
-        for i in range(n):
-            last_seen[s[i]]=i
-            #if(last_seen.get('a',0)!=-1 and last_seen.get('b',0)!=-1 and last_seen.get('c',0)!=-1):
-            #cnt=cnt+1+min(last_seen.get('a',0), last_seen.get('b',0), last_seen.get('c',0))
-            cnt=cnt+1+min(last_seen.values())
-        return cnt
+        k=3
+        return self.NSS(s, k)-self.NSS(s, k-1)
+        # n=len(s)
+        # cnt=0
+        # last_seen={'a':-1, 'b':-1, 'c':-1}
+        # for i in range(n):
+        #     last_seen[s[i]]=i
+        #     #if(last_seen.get('a',0)!=-1 and last_seen.get('b',0)!=-1 and last_seen.get('c',0)!=-1):
+        #     #cnt=cnt+1+min(last_seen.get('a',0), last_seen.get('b',0), last_seen.get('c',0))
+        #     cnt=cnt+1+min(last_seen.values())
+        # return cnt
 
 
-        # brute force
+     # brute forces
         # n=len(s)
         # cnt=0
         # for i in range(n):
@@ -25,4 +27,37 @@ class Solution(object):
         #         if(freq.get(0,0)+freq.get(1,0)+freq.get(2,0) == 3):
         #             cnt=cnt+(n-j)
         #             break
-        # return cnt;       
+        # return cnt;     
+
+        # n=len(s)
+        # cnt=0
+    
+        # for i in range(n):
+        #     visited=set()
+        #     for j in range(i,n):
+        #         visited.add(s[j])
+        #         if(len(visited)==3):
+        #             print(i,j)
+        #             cnt+=1
+        #         if(len(visited)>3):
+        #             break
+                
+        # return cnt
+    def NSS(self, s, k):
+        l=r=0
+        n=len(s)
+        freq={}
+        cnt=0
+
+        while r<n:
+
+            freq[s[r]]=freq.get(s[r],0)+1
+            while(len(freq)>k):
+                freq[s[l]]-=1
+                if(freq[s[l]]==0):
+                    del freq[s[l]]
+                l+=1
+            cnt+=r-l+1
+            r+=1
+        return cnt
+
