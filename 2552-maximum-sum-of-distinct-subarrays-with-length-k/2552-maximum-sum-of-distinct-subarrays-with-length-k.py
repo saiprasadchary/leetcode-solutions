ff1f66226dc01d2de698d0f7f4e6f0cbd35064a6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-22 13:55:00 -0500

    Time: 163 ms (36.26%), Space: 26.9 MB (28.36%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,30 +1,5 @@
 class Solution(object):
     def maximumSubarraySum(self, nums, k):
-        # n=len(nums)
-        # dict_wind={}
-        # wind_sum=0
-        # maxSum=0
-        # for i in range(n):
-        #     #considering every values into the dictionary
-        #     if nums[i] not in dict_wind:
-        #         dict_wind[nums[i]]=1
-        #         wind_sum+=nums[i]
-        #     else:
-        #         dict_wind[nums[i]]+=1
-        #         wind_sum+=nums[i]
-        #     #incrementing the sum of the window irrespective of its presence in dictionary
-
-        #     if i>=k:
-        #         #decrementing the values to be deleted in the dict in the wind-sum
-        #         wind_sum-=nums[i-k]
-        #         dict_wind[nums[i-k]]-=1
-        #         #removing its values 0 too, to clear the dictionary of unnecessary keys
-        #         if dict_wind[nums[i-k]]==0:
-        #             del dict_wind[nums[i-k]]
-        #     if len(dict_wind)==k:
-        #         maxSum=max(maxSum, wind_sum)
-        # return maxSum
-
         n=len(nums)
         freq={}
         left=0
@@ -35,7 +10,7 @@ class Solution(object):
             freq[nums[i]]=freq.get(nums[i],0)+1
             wind_sum+=nums[i]
 
-            if(i>=k):
+            if(i-left>=k):
                 wind_sum-=nums[left]
                 freq[nums[left]]-=1
                 if(freq[nums[left]]==0):
@@ -47,6 +22,8 @@ class Solution(object):
                 
         return maxSum
 
+###### the natural intuition that comes to mind is that the repetative values are first removed and if the windsize>k then size is adjusted accordingly######
+
     #     n=len(nums)
     #     l=r=0
     #     maxSum=0
@@ -55,8 +32,7 @@ class Solution(object):
 
     #     while(r<n):
 
-
-    # # increment freq for nums[r]
+    #   increment freq for nums[r]
     #         freq[nums[r]] = freq.get(nums[r], 0) + 1
     #         sum1 += nums[r]
 
@@ -76,7 +52,7 @@ class Solution(object):
     #                 del freq[nums[l]]
     #             l += 1
 
-    #         # check if window size = k
+    #         # check if window size = k could also check with freq len
     #         if (r - l + 1) == k:
     #             maxSum = max(maxSum, sum1)
 
