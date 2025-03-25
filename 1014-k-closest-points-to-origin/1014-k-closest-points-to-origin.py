1cafcc43de124bb06bf9e614bc90176c4ad37156
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-24 23:48:01 -0500

    Time: 151 ms (22.95%), Space: 17.9 MB (67.1%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -5,21 +5,50 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        n=len(points)
         heap=[]
-        for i in range(len(points)):
+        for i in range(n):
             x1=points[i][0]
             y1=points[i][1]
-            sum1=x1**2 + y1**2
-            distance=math.sqrt(sum1)
-            heapq.heappush(heap, (-distance, points[i]))
-            if len(heap)>k:
+            dist=sqrt(x1**2 + y1**2)
+            heapq.heappush(heap, (-dist, x1, y1))
+
+            if(len(heap)>k):
                 heapq.heappop(heap)
-        
-        res=[]
+        j=0
         while heap:
-            dist, point = heapq.heappop(heap)
-            res.append(point)
-        return res
+            _, x, y = heapq.heappop(heap)
+
+            points[j]=[x,y]
+            j+=1
+        return points[:j]
+
+
+
+
+
+
+
+
+
+
+
+
+        # heap=[]
+        # for i in range(len(points)):
+        #     x1=points[i][0]
+        #     y1=points[i][1]
+        #     sum1=x1**2 + y1**2
+        #     distance=math.sqrt(sum1)
+        #     heapq.heappush(heap, (-distance, points[i]))
+        #     if len(heap)>k:
+        #         heapq.heappop(heap)
+        
+        # res=[]
+        # while heap:
+        #     dist, point = heapq.heappop(heap)
+        #     res.append(point)
+        # return res
 
 
         
