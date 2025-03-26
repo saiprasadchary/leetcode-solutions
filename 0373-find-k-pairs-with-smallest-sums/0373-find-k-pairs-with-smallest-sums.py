5f7163726d21e187f763c78982e4117b3614bec6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-25 19:10:12 -0500

    Time: 157 ms (68.1%), Space: 27.7 MB (19.66%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -9,30 +9,59 @@ class Solution(object):
         n=len(nums1)
         m=len(nums2)
         heap=[]
-        visited=set()
         i=j=0
-        heapq.heappush(heap, ((nums1[i]+nums2[j]), i,j))
+        heapq.heappush(heap, ((nums1[0] + nums2[0]),i, j))
+        visited=set()
         visited.add((i,j))
         res=[]
-        #The heap and set are initialized based on which the while loop works as heap is not empty and visited to track any redundant
-
-
+        
         while k>0 and heap:
-            _,i,j=heapq.heappop(heap)
-            res.append([nums1[i], nums2[j]])
-            
-            #neighbour checks by incrementing (i+1,j) and (i,j+1) to push them into the minheap to extract the minimum of existing and newly pushed values 
-            if(i+1<n and (i+1, j) not in visited):
+            _, i, j = heapq.heappop(heap)
+            res.append([nums1[i],nums2[j]])
+
+            if i+1<n and (i+1, j) not in visited:
                 heapq.heappush(heap, ((nums1[i+1]+nums2[j]), i+1, j))
-                # redundancy checks
-                visited.add((i+1,j))
+                visited.add((i+1, j))
             
-            if(j+1<m and (i,j+1) not in visited):
+            if j+1< m and (i, j+1) not in visited:
+
                 heapq.heappush(heap, ((nums1[i]+nums2[j+1]), i, j+1))
-                visited.add((i,j+1))
-            # everytime we get a oair we decrement the k val
+                visited.add((i, j+1))
+        
             k-=1
         return res
+
+
+        
+        # n=len(nums1)
+        # m=len(nums2)
+        # heap=[]
+        # visited=set()
+        # i=j=0
+        # heapq.heappush(heap, ((nums1[i]+nums2[j]), i,j))
+        # visited.add((i,j))
+        # res=[]
+        # #The heap and set are initialized based on which the while loop works as heap is not empty and visited to track any redundant
+
+        # while k>0 and heap:
+        #     _,i,j=heapq.heappop(heap)
+        #     res.append([nums1[i], nums2[j]])
+            
+        #     #neighbour checks by incrementing (i+1,j) and (i,j+1) to push them into the minheap to extract the minimum of existing and newly pushed values 
+        #     if(i+1<n and (i+1, j) not in visited):
+        #         heapq.heappush(heap, ((nums1[i+1]+nums2[j]), i+1, j))
+        #         # redundancy checks
+        #         visited.add((i+1,j))
+            
+        #     if(j+1<m and (i,j+1) not in visited):
+        #         heapq.heappush(heap, ((nums1[i]+nums2[j+1]), i, j+1))
+        #         visited.add((i,j+1))
+        #     # everytime we get a oair we decrement the k val
+        #     k-=1
+        # return res
+
+
+
 
 
         ############## Better Brute Force Approach ######
