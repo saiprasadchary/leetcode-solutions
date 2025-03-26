5207ff74c81a4ab737019b72ec3a1fcd936a4922
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-03-26 15:34:17 -0500

    Time: 55 ms (46.24%), Space: 15.3 MB (28.01%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -5,14 +5,30 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-###### Brute force's #####
+        #Optimal Approach
         n=len(matrix)
-        temp=[]
-        for i in range(n):
-            temp.extend(matrix[i])
-        temp.sort()
-        return temp[k-1]
+        heap=[]
+        # consider the min of k and n ex: if n=3 and k=2, therefore the ele/val should be in the first nested list i.e n=1 or in the second nested list n=2, as they are in the ascending order
+        for i in range(min(k,n)):
+            heapq.heappush(heap, (matrix[i][0], i, 0))
+        # accumulating the first values of each list and allso their respective row and col 
 
+        for j in range(k):
+            val, row, col = heapq.heappop(heap)
+            if(col+1< n):
+                heapq.heappush(heap, (matrix[row][col+1], row, col+1))
+        return val
+        
+
+
+
+###### Brute force's #####
+        # n=len(matrix)
+        # temp=[]
+        # for i in range(n):
+        #     temp.extend(matrix[i])
+        # temp.sort()
+        # return temp[k-1]
 
 
 
