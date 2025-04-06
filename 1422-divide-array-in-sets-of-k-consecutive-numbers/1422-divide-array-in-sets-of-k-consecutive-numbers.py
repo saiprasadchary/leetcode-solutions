3126f2351d2b975efb6a867e4d496509982fdbda
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-06 12:01:39 -0500

    Time: 113 ms (91.3%), Space: 27.4 MB (27.54%) - LeetHub

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.py
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -8,16 +8,18 @@ class Solution(object):
         freq={}
         for i in nums:
             freq[i]=freq.get(i,0)+1
+            
         freq_keys=sorted(freq.keys())
 
         for val in freq_keys:
 
             while freq[val]:
-                for j in range(k):
+                for j in range(1, k):
                     curr=val+j
                     if(curr not in freq) or (freq[curr] == 0):
                         return False
-                    freq[curr]-=1       
+                    freq[curr]-=1   
+                freq[val]-=1    
         return True
 
 
