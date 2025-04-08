5be2b33a94cc80b4800287492c71c4d679bca64b
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-08 12:34:35 -0500

    Time: 51 ms (79.07%), Space: 13.5 MB (44.19%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,30 +1,28 @@
 class Solution(object):
     def isPossible(self, nums):
-        # Build frequency dictionary.
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+        freq={}
+        n=len(nums)
+        vacancy={}
+        for k in nums:
+            freq[k]=freq.get(k,0)+1
 
-        vacancy = {}  # counts of subsequences waiting for a specific number
-        
-        for num in nums:
-            if freq.get(num, 0) == 0:
+        for i in range(n):
+            if(freq[nums[i]]==0):
                 continue
 
-            # If there's a subsequence waiting for num, extend that subsequence:
-            if vacancy.get(num, 0) > 0:
-                vacancy[num] -= 1
-                # When num is added to a subsequence, the subsequence now expects num+1.
-                vacancy[num + 1] = vacancy.get(num + 1, 0) + 1
-                freq[num] -= 1
-            # Otherwise, try to create a new subsequence starting from num
-            elif freq.get(num, 0) > 0 and freq.get(num + 1, 0) > 0 and freq.get(num + 2, 0) > 0:
-                freq[num] -= 1
-                freq[num + 1] -= 1
-                freq[num + 2] -= 1
-                # New subsequence now expects num+3.
-                vacancy[num + 3] = vacancy.get(num + 3, 0) + 1
+            if(vacancy.get(nums[i],0)>0):
+                vacancy[nums[i]]-=1
+                vacancy[nums[i]+1]=vacancy.get(nums[i]+1, 0)+1
+                freq[nums[i]]-=1
+            
+            elif freq.get(nums[i], 0) > 0 and freq.get(nums[i] + 1, 0) > 0 and freq.get(nums[i] + 2, 0) > 0:                
+                freq[nums[i]]-=1
+                freq[nums[i]+1]-=1
+                freq[nums[i]+2]-=1
+
+                vacancy[nums[i]+3]=vacancy.get(nums[i]+3, 0)+1
+
             else:
                 return False
-        
         return True
+            
