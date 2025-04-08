8bbfa190140c4bee247520e7500753f5bc7de296
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-08 14:13:18 -0500

    Time: 2100 ms (5.01%), Space: 27.5 MB (20.35%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,58 +1,23 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        
-        if len(nums)==0:
+        #bruteforce
+
+        n=len(nums)
+        maxlen=1
+        cnt=1
+        if not nums:
             return 0
-        longest=1;
-        nums_s=set(nums);
-        print(nums_s)
-        for i in range(len(nums)):
-            if nums[i]-1 not in nums_s:
-                c=1;
-                j=nums[i]+1
-                while j in nums_s:
-                    c+=1;
-                    j+=1
-                    longest=max(longest, c);
-        return longest;
-                    
-                
-
-
-
-
-        # #brute force approach
-        # if len(nums)==0:
-        #     return 0
-        # longest=1;
-        # c=1
-        # s_nums=sorted(nums);
-        # print(s_nums)
-        # for i in range(1, len(nums)):
-        #     start=s_nums[i]
-        #     if s_nums[i]==s_nums[i-1]:
-        #         continue;
-        #     elif s_nums[i-1]+1==s_nums[i]:
-        #         c+=1;
-        #         print(c)
-        #         longest=max(longest,c);
-        #     else:
-        #         c=1
-            
-        # return longest
-            
-
-        # s=set(nums);
-        # longest=0;
-        # for i in range(len(nums)):
-        #     if nums[i]-1 not in s:
-        #         c=1;
-        #         k=nums[i]+1
-
-        #         while k in s:
-        #             c+=1;
-        #             k+=1
-        #         longest=max(longest,c);
-        # return longest;
+        if n==1:
+            return 1
+        nums=list(set(nums))
+        nums.sort()
+        for i in range(1, len(nums)):
+            if(nums[i-1]+1==nums[i]):
+                cnt+=1
+                print(nums[i-1], nums[i])
+                maxlen=max(cnt, maxlen)
+            else:
+                cnt=1
+        return maxlen
 
         
