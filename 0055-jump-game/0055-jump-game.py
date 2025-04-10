f0082dac9aaf179c32c7290c0183879bd20da9d6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-10 18:21:10 -0500

    Time: 52 ms (15.01%), Space: 13.4 MB (24.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,15 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        max_index=0
+        
         n=len(nums)
+        if n==1:
+            return True
+
+        maxind=0
         for i in range(n):
-            if(i>max_index):
+            if(i>maxind):
                 return False
-            max_index=max(max_index, i+nums[i])
-            if(max_index>=n):
-                return True
+            maxind=max(maxind, i+nums[i])
         return True
+
+
+
+
+
+
+        # max_index=0
+        # n=len(nums)
+        # for i in range(n):
+        #     if(i>max_index):
+        #         return False
+        #     max_index=max(max_index, i+nums[i])
+        #     if(max_index>=n):
+        #         return True
+        # return True
 
 
             
