a54397b5c392126efab7ad38f48333da4e4c76eb
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-13 23:14:57 -0500

    Time: 11 ms (49.31%), Space: 14.7 MB (6.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,48 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        candidate=0;
-        count=0;
-
-        for curr_num in nums:
-            if count==0:
-                candidate=curr_num;
-            if curr_num==candidate:
-                count+=1;
+        cand=0
+        cnt=0
+        n=len(nums)
+        for i in range(n):
+            if cnt==0:
+                cand=nums[i]
+            if(cand==nums[i]):
+                cnt+=1
             else:
-                count-=1
-        return candidate;
-
-
-
-
-        # dict={};
-        # for i in  nums:
-        #     if i not in dict:
-        #         dict[i]=1;
-        #     else:
-        #         dict[i]+=1;
-        # ans=-1
-        # max_val = float('-inf')
-        # for k , v in dict.items():
-        #     if v>max_val:
-        #         max_val=v;
-        #         ans=k;
-        # return ans
-
-        
-        # if nums is None:
-        #     return None;
-        # s=set(nums);
-        # c=0
-        # c1=0;
-        # candidate=nums[0]
-        # for i in nums:
-        #     if i in s:
-        #         c = nums.count(i) 
-        #         if c > c1:
-        #             c1 = c
-        #             candidate = i
-        #         s.remove(i)
-        # return candidate;
-
+                cnt-=1
+        return cand
