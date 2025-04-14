426aa0b5fed6d4518785cc96528902589f9d3e5f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-14 00:08:05 -0500

    Time: 6 ms (58.01%), Space: 14.5 MB (6.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -10,37 +10,35 @@ class Solution(object):
         cnt2=0
         n=len(nums)
         for i in range(n):
-            if(nums[i]==cand1):
-                cnt1+=1
-            elif(nums[i]==cand2):
-                cnt2+=1
-            
-            elif(cnt1==0):
+            if(cnt1==0) and nums[i]!=cand2:
                 cand1=nums[i]
                 cnt1+=1
-            elif(cnt2==0 and cand1!=nums[i]):
+            elif(cnt2==0) and nums[i]!=cand1:
                 cand2=nums[i]
+                cnt2+=1
+            elif(nums[i]==cand1):
+                cnt1+=1
+            elif(nums[i]==cand2):
                 cnt2+=1
             else:
                 cnt1-=1
                 cnt2-=1
-            
-        c1=0
-        c2=0
-        print(cand1)
-        print(cand2)
-        for i in range(n):
-            if nums[i]==cand1:
-                c1+=1
-            if(nums[i]==cand2):
-                c2+=1
+        
+        cnt1=0
+        cnt2=0
+        for j in range(n):
+            if nums[j]==cand1:
+                cnt1+=1
+            elif nums[j]==cand2:
+                cnt2+=1
 
-        l1=[]   
-        if c1>(len(nums)/3):
-            l1.append(cand1)
-        if c2>(len(nums)/3) and cand1!=cand2:
-            l1.append(cand2)
-        return l1
+        res=[]
+        print(cnt1, cnt2)
+        if(cnt1>n/3):
+            res.append(cand1)
+        if(cnt2>n/3) and cnt2!=0:
+            res.append(cand2)
+        return res
     
 
         
