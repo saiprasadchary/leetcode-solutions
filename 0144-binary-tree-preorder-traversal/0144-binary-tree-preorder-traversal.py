66fba2aed2f018bc007b6dcccd23eab716204e59
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-04-18 13:49:43 -0500

    Time: 0 ms (100%), Space: 12.4 MB (93.24%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -5,28 +5,43 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+   
     def preorderTraversal(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
-        # Initialize the result list on the instance
-        self.res = []
-        # Kick off the real recursion
-        self._preorder(root)
-        return self.res
+        res=[]
+        def dfs(node):
+            if(node==None):
+                return
+            res.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return res
+
+# another approach
+
+    # def preorderTraversal(self, root):
+    #     """
+    #     :type root: Optional[TreeNode]
+    #     :rtype: List[int]
+    #     """
+    #     # Initialize the result list on the instance
+    #     self.res = []
+    #     # Kick off the real recursion
+    #     self._preorder(root)
+    #     return self.res
     
-    def _preorder(self, node):
-        # Base case
-        if node is None:
-            return
-        # Visit
-        self.res.append(node.val)
-        # Recurse left, then right
-        self._preorder(node.left)
-        self._preorder(node.right)
-
-
+    # def _preorder(self, node):
+    #     # Base case
+    #     if node is None:
+    #         return
+    #     # Visit
+    #     self.res.append(node.val)
+    #     # Recurse left, then right
+    #     self._preorder(node.left)
 
 
         
