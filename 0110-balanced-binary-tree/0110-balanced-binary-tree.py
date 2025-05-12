ad8c8af940a6581ea326830272ed0615fc43c20f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-12 16:24:02 -0500

    Time: 9 ms (34.39%), Space: 17.9 MB (41.8%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -14,42 +14,37 @@ class Solution(object):
         # I'll utilize the height of BT and manipualte its code to make it working for this
         
         #base case
-        def check(node):
-            if(node is None):
-                return 0
-            lh=check(node.left)
-            rh=check(node.right)
+        # def check(node):
+        #     if(node is None):
+        #         return 0
+        #     lh=check(node.left)
+        #     rh=check(node.right)
 
-            if(lh==-1 or rh==-1):
-                return -1
-            if(abs(lh-rh)>1): 
-                return -1
+        #     if(lh==-1 or rh==-1):
+        #         return -1
+        #     if(abs(lh-rh)>1): 
+        #         return -1
 
-            return 1+max(lh, rh)
+        #     return 1+max(lh, rh)
 
-        if check(root)!=-1:
-            return True
-        else:
-            return False
+        # if check(root)!=-1:
+        #     return True
+        # else:
+        #     return False
 
         def height_check(root):
             if root is None:
                 return 0
             left=1+height_check(root.left)
             right=1+height_check(root.right)
-            if abs(l-r)>1:
+            if(left==0 or right==0):
                 return -1
+            if abs(left-right)>1:
+                return -1 # which inturn gets assigns to the upper level, either for the right or left
             return max(left, right)
 
-        if root is None:
-            return True
-        else:
-            l=height_check(root.left)
-            r=height_check(root.right)
-            print(l,r)
-            if abs(l-r)>1:
-                return False
-            return True
+        return height_check(root)!=-1
+            
 
 
         
