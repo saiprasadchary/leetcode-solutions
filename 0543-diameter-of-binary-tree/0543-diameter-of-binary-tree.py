afa54e207e803ac356f1a42c7ded3ce41891ecc4
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-12 19:21:28 -0500

    Time: 11 ms (70.62%), Space: 26.2 MB (68.38%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -10,14 +10,18 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+
+        #note: diameter need not include the root node of the tree
         if root == None:
             return 0
-        self.maxi=0
+        self.maxi=0# this variable is attached in each and every iteration, and updated accordingly
+
         def maxdia(node):
             if(node==None):
                 return 0
             lh=maxdia(node.left)
             rh=maxdia(node.right)
+            
             self.maxi=max(self.maxi, lh+rh)
 
             return 1+max(lh,rh)
@@ -25,5 +29,4 @@ class Solution(object):
         maxdia(root)
         
         return self.maxi
-
-        
+       
