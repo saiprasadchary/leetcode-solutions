56ffb004f5288732c6a000be35da1b5013fe04ab
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-21 21:44:32 -0500

    Time: 0 ms (100%), Space: 12.5 MB (31.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,19 +7,38 @@
 class Solution(object):
     def rightSideView(self, root):
         
-        self.res=[]
-        level=0
-        def rightView(node, level):
-            if node is None:
-                return
+        # self.res=[]
+        # level=0
+        # def rightView(node, level):
+        #     if node is None:
+        #         return
             
-            if(len(self.res)==level):
-                self.res.append(node.val)
+        #     if(len(self.res)==level):
+        #         self.res.append(node.val)
             
-            rightView(node.right, level+1)
-            rightView(node.left, level+1)
+        #     rightView(node.right, level+1)
+        #     rightView(node.left, level+1)
         
-        rightView(root, level)
-        return self.res
+        # rightView(root, level)
+        # return self.res
+
+        if root is None:
+            return []
+        res=[]
+        que=deque()
+        que.append(root)
+        while que:
+            n=len(que)
+
+            for col in range(n):
+                node=que.popleft()
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            res.append(node.val)
+
+        return res
+
 
         
