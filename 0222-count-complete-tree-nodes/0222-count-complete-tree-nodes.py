08f929ee04f23ebedc9aca2b31ba30acced65707
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-26 15:38:55 -0500

    Time: 12 ms (35.82%), Space: 28.3 MB (63.27%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -10,20 +10,30 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        self.count=0
+        def DFS(node):
+            if node is None:
+                return
+            self.count+=1
+            DFS(node.left)
+            DFS(node.right)
+        DFS(root)
+        return self.count
+
         
-        que=deque()
-        que.append(root)
-        cnt=0
-        if root is None:
-            return 0
-        while que:
-            n=len(que)
-            for i in range(n):
-                node=que.popleft()
-                cnt+=1
-                if node.left:
-                    que.append(node.left)
-                if node.right:
-                    que.append(node.right)
-        return cnt
+        # que=deque()
+        # que.append(root)
+        # cnt=0
+        # if root is None:
+        #     return 0
+        # while que:
+        #     n=len(que)
+        #     for i in range(n):
+        #         node=que.popleft()
+        #         cnt+=1
+        #         if node.left:
+        #             que.append(node.left)
+        #         if node.right:
+        #             que.append(node.right)
+        # return cnt
         
