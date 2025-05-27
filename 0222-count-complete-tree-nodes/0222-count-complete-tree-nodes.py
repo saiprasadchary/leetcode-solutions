73310585b69999e97a482301ded6ecbc0b610d68
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-26 22:54:24 -0500

    Time: 0 ms (100%), Space: 28.3 MB (28.81%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,22 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+
     def countNodes(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        self.count=0
-        def DFS(node):
-            if node is None:
-                return
-            self.count+=1
-            DFS(node.left)
-            DFS(node.right)
-        DFS(root)
-        return self.count
-
+        if root is None:
+            return 0
+        lh=self.getLeftNode(root)
+        rh=self.getRightNode(root)
         
+        if(lh==rh):
+            return pow(2, lh)-1
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
+
+    def getLeftNode(self, node):
+            if node is None:
+                return 0
+            return 1+self.getLeftNode(node.left)
+        
+    def getRightNode(self, node):
+        if node is None:
+            return 0
+        return 1+self.getRightNode(node.right)
+        
+
+        # basic DFS and BFS approaches are below:
+
+        # self.count=0
+        # def DFS(node):
+        #     if node is None:
+        #         return
+        #     self.count+=1
+        #     DFS(node.left)
+        #     DFS(node.right)
+        # DFS(root)
+        # return self.count
+
+    
         # que=deque()
         # que.append(root)
         # cnt=0
