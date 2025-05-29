5d2d9ca452cc68943b27305fd87fb1f43c459f39
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-29 11:56:23 -0500

    Time: 0 ms (100%), Space: 12.7 MB (0.4%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -48,23 +48,58 @@ class Solution(object):
     #     self.DFS(node.left)
     #     self.res.append(node.val)
     #     self.DFS(node.right)
-    
-        res=[]
-        if root is None:
-            return res
-        #initially stack is empty
-        stack=[]
+
+
+    #stack based solution
+        # res=[]
+        # if root is None:
+        #     return res
+        # #initially stack is empty
+        # stack=[]
+        # node=root
+        # while stack or node:
+        #     #move left untill the leaf node and null are found
+        #     if node is not None:
+        #         stack.append(node)
+        #         node=node.left
+        #     else:
+        #         node=stack.pop()
+        #         res.append(node.val)
+        #         node=node.right
+        # return res
+
         node=root
-        while stack or node:
-            #move left untill the leaf node and null are found
-            if node is not None:
-                stack.append(node)
-                node=node.left
-            else:
-                node=stack.pop()
+        res=[]
+
+        while node:
+            if node.left is None:
                 res.append(node.val)
                 node=node.right
+            else:
+                curr=node.left
+                while curr.right and curr.right!=node:
+                    curr=curr.right
+                if curr.right is None:
+                    #thread establishing
+                    curr.right=node
+                    node=node.left
+                #thread link alreadily exists
+                else:
+                    curr.right = None
+                    res.append(node.val)
+                    node=node.right
+
         return res
+
+
+                
+
+
+
+                
+
+
+
         
 
 
