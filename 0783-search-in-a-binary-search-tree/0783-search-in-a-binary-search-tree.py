b6a803f378090399f1cc33919ac787cace9d4733
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-05-30 14:43:06 -0500

    Time: 0 ms (100%), Space: 16.4 MB (93.72%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -6,14 +6,25 @@
 #         self.right = right
 class Solution(object):
     def searchBST(self, root, val):
+        # using while:
+        while root:
+            if root.val ==val:
+                return root
+            if root.val<val:
+                root=root.right
+            else:
+                root=root.left
+        return None
+
+
 
         #DFS similar approach
-        if root is None or root.val==val:
-            return root
-        if val<root.val:
-            return self.searchBST(root.left, val)
-        else:
-            return self.searchBST(root.right, val)
+        # if root is None or root.val==val:
+        #     return root
+        # if val<root.val:
+        #     return self.searchBST(root.left, val)
+        # else:
+        #     return self.searchBST(root.right, val)
             
       
 
