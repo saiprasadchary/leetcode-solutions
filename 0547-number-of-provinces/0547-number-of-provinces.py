3412eead2a36565c13ce1cf408c1214ff09e27dd
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-08-26 15:52:57 -0400

    Time: 16 ms (20.84%), Space: 13.1 MB (54.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def dfs(self, node, visited, adjList):
-        visited[node] = True
+        visited[node] = 1
+        
         for neighbor in adjList[node]:
             if not visited[neighbor]:
                 self.dfs(neighbor, visited, adjList)
 
     def findCircleNum(self, isConnected):
         V = len(isConnected)
-        adjList = [[] for _ in range(V)]
+        
+        adjList=[]
+
+        for x in range(V):
+            adjList.append([])
 
         # Build adjacency list from adjacency matrix
         for i in range(V):
@@ -16,11 +21,11 @@ class Solution(object):
                     adjList[i].append(j)
                     adjList[j].append(i)
 
-        visited = [False] * V
+        visited = [0] * V
         res = 0
 
         for i in range(V):
-            if not visited[i]:
+            if visited[i] == 0:
                 self.dfs(i, visited, adjList)
                 res += 1  # one connected component found
 
