953502d84aaabe6aeb554518acb2f389cfbb06cb
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-08-27 14:19:31 -0400

    Time: 0 ms (100%), Space: 12.6 MB (26.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,8 +5,25 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if x==0:
-            return 0
-        if n==0:
-            return 1
-        return x**n
+        def helper(x,n):
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            
+            res=helper(x, n//2)
+            res=res*res
+            return res if n%2==0 else x*res
+
+        res=helper(x, abs(n))
+        return res if n>0 else 1/res
+
+
+
+
+        # the common solution that comes is:
+        # if x==0:
+        #     return 0
+        # if n==0:
+        #     return 1
+        # return x**n
