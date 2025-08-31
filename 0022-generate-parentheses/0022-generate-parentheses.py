e0374f2c060426ba5cead0e3859aad329d21bac6
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-08-31 19:29:59 -0400

    Time: 4 ms (24.55%), Space: 12.6 MB (94.35%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,33 @@
+class Solution(object):
+
+    def helper(self,n, opened, closed, curr, res):
+        if opened ==0 and closed==0:
+            res.append(curr)
+        
+        if not curr or opened:          
+            self.helper(n, opened-1, closed, curr+"(", res)
+        
+        if curr and curr[-1]==")" and opened==closed:
+            pass
+
+        elif curr and curr[-1]==")" and opened<closed:
+            self.helper(n, opened, closed-1, curr+")", res)
+
+        elif curr and closed:
+            self.helper(n, opened, closed-1, curr+")", res)
+
+
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        opened=n
+        closed=n
+        curr=""
+        res=[]
+        self.helper(n, opened, closed, curr, res)
+        return res
+
+         
+        
