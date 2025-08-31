4a63f9d26586d5ecb5620d109ed1401a9fd6753b
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-08-31 19:55:00 -0400

    Time: 4 ms (24.55%), Space: 12.8 MB (48.37%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,20 +1,23 @@
 class Solution(object):
 
-    def helper(self,n, opened, closed, curr, res):
+    def helper(self, opened, closed, curr, res):
         if opened ==0 and closed==0:
             res.append(curr)
         
         if opened:          
-            self.helper(n, opened-1, closed, curr+"(", res)
+            self.helper(opened-1, closed, curr+"(", res)
+
+        if opened<closed:
+            self.helper(opened, closed-1, curr+")", res)
         
-        if curr and curr[-1]==")" and opened==closed:
-            pass
+        # if curr and curr[-1]==")" and opened==closed:
+        #     pass
 
-        elif curr and curr[-1]==")" and opened<closed:
-            self.helper(n, opened, closed-1, curr+")", res)
+        # elif curr and curr[-1]==")" and opened<closed:
+        #     self.helper(n, opened, closed-1, curr+")", res)
 
-        elif curr and closed:
-            self.helper(n, opened, closed-1, curr+")", res)
+        # elif curr and closed:
+        #     self.helper(n, opened, closed-1, curr+")", res)
 
 
     def generateParenthesis(self, n):
@@ -26,7 +29,7 @@ class Solution(object):
         closed=n
         curr=""
         res=[]
-        self.helper(n, opened, closed, curr, res)
+        self.helper(opened, closed, curr, res)
         return res
 
          
