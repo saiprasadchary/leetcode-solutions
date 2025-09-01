84bfc9cb8bcfaf7ffcff6c0c2964985abf7b7140
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-09-01 12:23:10 -0400

    Time: 0 ms (100%), Space: 12.6 MB (49.08%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def dfs_helper(self, nums, i, sublist, op):
+        # hitting the base case with the branch length exceeded or equals to the length(language wise)
+        if i==len(nums):
+            op.append(sublist[:])
+            return
+        
+        #pick case
+        sublist.append(nums[i])
+        self.dfs_helper(nums, i+1, sublist, op)
+
+        # backtrack by removing the last added element to get the parent and travel to the desired side
+        sublist.pop()
+
+        #no pick
+        self.dfs_helper(nums, i+1, sublist, op)
+        
+
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        op=[]
+        sublist=[]
+        self.dfs_helper(nums, 0, sublist, op)
+        return op
+
+        
