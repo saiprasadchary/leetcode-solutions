e91dbe5ac7c6aac1c932edaad2f27cdc77173df0
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-09-06 18:11:34 -0400

    Time: 0 ms (100%), Space: 12.3 MB (98.88%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+
+    def dfs_helper(self, element, arr, subset, res, total, k):
+        if len(subset)==k:
+            if total==0:
+                res.append(subset[:])
+            else:
+                return
+        
+        for ele in range(element, len(arr)+1):
+            
+            subset.append(ele)
+            self.dfs_helper(ele+1, arr, subset, res, total-ele, k)
+            subset.pop()
+
+
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        subset=[]
+        arr=[1,2,3,4,5,6,7,8,9]
+        self.dfs_helper(1, arr, subset, res, n, k)
+        return res
+        
