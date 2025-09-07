f40da568afbebfa37cdb81c80dc188e0d8eba7a2
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-09-07 19:08:43 -0400

    Time: 1269 ms (5.05%), Space: 44.4 MB (89.14%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,36 @@
+class Solution(object):
+
+    def IsPalindrome(self, partitions):
+        n=len(partitions)
+        for i in range(n//2):
+            if(partitions[i]!=partitions[n-i-1]):
+                return False
+        return True
+
+    def dfs_helper(self, ind, s, substr, res):
+
+        if(ind==len(s)):
+            print(substr)
+            res.append(substr[:])
+            return
+
+        for i in range(ind, len(s)):
+            partitions=s[ind:i+1]
+
+            if self.IsPalindrome(partitions):
+                substr.append(partitions)
+                self.dfs_helper(i+1, s, substr, res)
+                substr.pop()
+        
+        
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        res=[]
+        substr=[]
+        self.dfs_helper(0, s, substr, res)
+        return res
+
+        
