28e72de591f94029716757b5d4ea410635977471
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-10-13 23:45:16 -0400

    Time: 4 ms (89.22%), Space: 13.2 MB (48.43%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,46 +1,11 @@
 class Solution(object):
 
-    # #dfs function to check the 
-    # def dfs(self, node, visited, adjList):
-    #     visited[node] = 1
-    #     for neighbor in adjList[node]:
-    #         if not visited[neighbor]:
-    #             self.dfs(neighbor, visited, adjList)
-
-    # def findCircleNum(self, isConnected):
-    #     V = len(isConnected)
-    #     adjList=[]
-    #     for _ in range(V):
-    #         adjList.append([])
-
-    #     # Build adjacency list from adjacency matrix
-    #     for i in range(V):
-    #         for j in range(i+1, V):
-    #             if isConnected[i][j] == 1:
-    #                 adjList[i].append(j)
-    #                 adjList[j].append(i)
-
-    #     visited = [0] * V
-    #     res = 0
-
-    #     for i in range(V):
-    #         if visited[i] == 0:
-    #             self.dfs(i, visited, adjList)
-    #             res += 1  # one connected component found
-
-    #     return res
-
-
-    #bfs function to check the 
-    def bfs(self, node, visited, adjList):
-        que=deque()
-        que.append(node)
-        while que:
-            nd=que.popleft()
-            for i in adjList[nd]:
-                if visited[i]==0:
-                    que.append(i)
-                    visited[i]=1
+    #dfs function to check the adjList nodes/values
+    def dfs(self, node, visited, adjList):
+        visited[node] = 1
+        for neighbor in adjList[node]:
+            if not visited[neighbor]:
+                self.dfs(neighbor, visited, adjList)
 
     def findCircleNum(self, isConnected):
         V = len(isConnected)
@@ -60,7 +25,10 @@ class Solution(object):
 
         for i in range(V):
             if visited[i] == 0:
-                self.bfs(i, visited, adjList)
+                self.dfs(i, visited, adjList)
                 res += 1  # one connected component found
 
         return res
+
+
+ 
