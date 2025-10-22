2d4bb0839191cb9bb7bc6041e2be7ccd4f7f7577
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-10-22 01:56:59 -0400

    Time: 7 ms (5.19%), Space: 12.7 MB (46.97%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        ROWS=len(image)
+        COLS=len(image[0])
+        que=deque()
+        startColor=image[sr][sc]
+        
+        if image[sr][sc]==color:
+            return image
+        else:
+            image[sr][sc]=color
+
+        dimensions=[(0,1), (1,0), (-1,0), (0,-1)]
+        que.append((sr, sc))
+
+        while que:
+            r,c=que.popleft()
+            for ro, co in dimensions:
+                nr=ro+r
+                nc=co+c
+                if 0<=nr<ROWS and 0<=nc<COLS and image[nr][nc]==startColor:
+                    print(nr, nc)
+                    que.append((nr, nc))
+                    image[nr][nc]=color
+        return image
+
+
+        
