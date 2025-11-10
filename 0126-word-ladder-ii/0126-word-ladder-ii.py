0a438e132871f22b6ed36bad44b10084e9116c2f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-11-10 18:21:01 -0500

    Time: 18 ms (82.38%), Space: 13.7 MB (8.37%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -1,50 +1,46 @@
 from collections import defaultdict, deque
 
-class Solution(object):
+class Solution:
     def findLadders(self, beginWord, endWord, wordList):
         wordSet = set(wordList)
         if endWord not in wordSet:
             return []
 
-        L = len(beginWord)
+        neighbors = defaultdict(list)
+        for word in wordSet:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                neighbors[pattern].append(word)
+
+        level = {beginWord: 0}
         parents = defaultdict(set)
-        level = {beginWord}
-        found = False
+        queue = deque([beginWord])
 
-        while level and not found:
-            next_level = set()
-            wordSet -= level  # block revisits of this level in future levels
+        while queue:
+            word = queue.popleft()
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                for nei in neighbors[pattern]:
+                    if nei not in level:
+                        level[nei] = level[word] + 1
+                        queue.append(nei)
+                    if level[nei] == level[word] + 1:
+                        parents[nei].add(word)
 
-            for w in level:
-                arr = list(w)
-                for i in range(L):
-                    old = arr[i]
-                    for c in "abcdefghijklmnopqrstuvwxyz":
-                        if c == old: 
-                            continue
-                        arr[i] = c
-                        nw = ''.join(arr)
-                        if nw in wordSet:
-                            parents[nw].add(w)
-                            next_level.add(nw)
-                    arr[i] = old
-
-            if endWord in next_level:
-                found = True
-            level = next_level
-
-        if not found:
+        if endWord not in parents:
             return []
 
-        # backtrack all shortest paths from endWord to beginWord
-        res, path = [], [endWord]
-        def dfs(w):
-            if w == beginWord:
+        res = []
+        path = [endWord]
+
+        def dfs(word):
+            if word == beginWord:
                 res.append(path[::-1])
                 return
-            for p in parents[w]:
+            for p in parents[word]:
                 path.append(p)
                 dfs(p)
                 path.pop()
+
         dfs(endWord)
         return res
