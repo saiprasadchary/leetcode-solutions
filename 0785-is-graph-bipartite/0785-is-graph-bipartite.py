916ab2a4561d793037a82a94b19c5586f68b5b25
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2025-12-28 17:16:57 -0500

    Time: 3 ms (48.1%), Space: 12.7 MB (50.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,30 +1,59 @@
 class Solution(object):
-    from collections import deque
-    def isBipartite(self, graph):
-        """
-        :type graph: List[List[int]]
-        :rtype: bool
-        """
-        ROWS=len(graph)
-        if ROWS is None:
-            return True
-        visited=[-1]*ROWS
 
-        for node in range(ROWS):
-            if visited[node] !=-1:
-                continue
-            que=deque()
-            que.append(node)
-            visited[node]=0
-            while que:
-                curr=que.popleft()
-                for neighbor in graph[curr]:
-                    if visited[neighbor]==-1:
-                        visited[neighbor]=1-visited[curr]
-                        que.append(neighbor)
-                    elif visited[neighbor]==visited[curr]:
-                        return False
+    # BFS solution:
+    # from collections import deque
+    # def isBipartite(self, graph):
+    #     """
+    #     :type graph: List[List[int]]
+    #     :rtype: bool
+    #     """
+    #     ROWS=len(graph)
+    #     if ROWS is None:
+    #         return True
+    #     visited=[-1]*ROWS
+
+    #     for node in range(ROWS):
+    #         if visited[node] !=-1:
+    #             continue
+    #         que=deque()
+    #         que.append(node)
+    #         visited[node]=0
+    #         while que:
+    #             curr=que.popleft()
+    #             for neighbor in graph[curr]:
+    #                 if visited[neighbor]==-1:
+    #                     visited[neighbor]=1-visited[curr]
+    #                     que.append(neighbor)
+    #                 elif visited[neighbor]==visited[curr]:
+    #                     return False
+    #     return True
+
+    #DFS solution:
+    def dfs(self, node, col, graph, visited):
+        visited[node]=col
+        for neighbor in graph[node]:
+            if visited[neighbor]==-1:
+                if self.dfs(neighbor, 1-col, graph, visited)== False:
+                    return False
+            elif visited[neighbor]==col:
+                return False
         return True
+
+    def isBipartite(self, graph):
+        ROW=len(graph)
+        visited=[]
+        for i in range(ROW):
+            visited.append(-1)
+
+        for i in range(ROW):
+            if visited[i]==-1:
+                if self.dfs(i, 0, graph, visited)== False:
+                    return False 
+        return True
+        
+
+
+
 
 
 
